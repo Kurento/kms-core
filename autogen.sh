42022393e5053166e227221b62d7c5e9343b2990
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-03-20 09:16:42 +0100

    Add pre-commit hook
    Change-Id: Iec574a89f2c900307722bc601861c6245fd58d3e

--- autogen.sh
+++ autogen.sh
@@ -13,6 +13,13 @@ autoreconf --verbose --force --install --make || {
  exit 1;
 }
 
+# install pre-commit hook for doing clean commits
+if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \);
+then
+rm -f .git/hooks/pre-commit
+ln -s ../../hooks/pre-commit.hook .git/hooks/pre-commit
+fi
+
 echo
 echo "Now type 'make' to compile this module."
 echo
