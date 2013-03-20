1b743e67ec9b538ef562c2871af0a40eb668f551
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-03-20 13:24:53 +0100

    Add caps to element templates, only support audio and video
    Issue: #109
    Change-Id: I9a5c2576c3df4ab140f48bb5fb2995f0a7e07b1c

--- src/gstagnosticbin.c
+++ src/gstagnosticbin.c
@@ -9,6 +9,90 @@
 
 #define PLUGIN_NAME "agnosticbin"
 
+#define AUDIO_CAPS "audio/x-raw;"\
+  "audio/x-sbc;" \
+  "audio/x-mulaw;" \
+  "audio/x-flac;" \
+  "audio/x-alaw;" \
+  "audio/x-speex;" \
+  "audio/x-ac3;" \
+  "audio/x-alac;" \
+  "audio/mpeg,mpegversion=1,layer=2;" \
+  "audio/x-nellymoser;" \
+  "audio/x-gst_ff-sonic;" \
+  "audio/x-gst_ff-sonicls;" \
+  "audio/x-wma,wmaversion=1;" \
+  "audio/x-wma,wmaversion=2;" \
+  "audio/x-dpcm,layout=roq;" \
+  "audio/x-adpcm,layout=adx;" \
+  "audio/x-adpcm,layout=g726;" \
+  "audio/x-adpcm,layout=quicktime;" \
+  "audio/x-adpcm,layout=dvi;" \
+  "audio/x-adpcm,layout=microsoft;" \
+  "audio/x-adpcm,layout=swf;" \
+  "audio/x-adpcm,layout=yamaha;" \
+  "audio/mpeg,mpegversion=4;" \
+  "audio/mpeg,mpegversion=1,layer=3;" \
+  "audio/x-celt;" \
+  "audio/mpeg,mpegversion=[2, 4];" \
+  "audio/x-vorbis;" \
+  "audio/x-opus;" \
+  "audio/AMR,rate=[8000, 16000],channels=1;" \
+  "audio/x-gsm;"
+
+#define VIDEO_CAPS "video/x-raw;" \
+  "video/x-dirac;" \
+  "image/png;" \
+  "image/jpeg;" \
+  "video/x-smoke;" \
+  "video/x-asus,asusversion=1;" \
+  "video/x-asus,asusversion=2;" \
+  "image/bmp;" \
+  "video/x-dnxhd;" \
+  "video/x-dv;" \
+  "video/x-ffv,ffvversion=1;" \
+  "video/x-gst_ff-ffvhuff;" \
+  "video/x-flash-screen;" \
+  "video/x-flash-video,flvversion=1;" \
+  "video/x-h261;" \
+  "video/x-h263,variant=itu,h263version=h263;" \
+  "video/x-h263,variant=itu,h263version=h263p;" \
+  "video/x-huffyuv;" \
+  "image/jpeg;" \
+  "image/jpeg;" \
+  "video/mpeg,mpegversion=1;" \
+  "video/mpeg,mpegversion=2;" \
+  "video/mpeg,mpegversion=4;" \
+  "video/x-msmpeg,msmpegversion=41;" \
+  "video/x-msmpeg,msmpegversion=42;" \
+  "video/x-msmpeg,msmpegversion=43;" \
+  "video/x-gst_ff-pam;" \
+  "image/pbm;" \
+  "video/x-gst_ff-pgm;" \
+  "video/x-gst_ff-pgmyuv;" \
+  "image/png;" \
+  "image/ppm;" \
+  "video/x-rle,layout=quicktime;" \
+  "video/x-gst_ff-roqvideo;" \
+  "video/x-pn-realvideo,rmversion=1;" \
+  "video/x-pn-realvideo,rmversion=2;" \
+  "video/x-gst_ff-snow;" \
+  "video/x-svq,svqversion=1;" \
+  "video/x-wmv,wmvversion=1;" \
+  "video/x-wmv,wmvversion=2;" \
+  "video/x-gst_ff-zmbv;" \
+  "video/x-theora;" \
+  "video/x-h264;" \
+  "video/x-gst_ff-libxvid;" \
+  "video/x-h264;" \
+  "video/x-xvid;" \
+  "video/mpeg,mpegversion=[1, 2];" \
+  "video/x-theora;" \
+  "video/x-vp8;" \
+  "application/x-yuv4mpeg,y4mversion=2;" \
+
+#define AGNOSTIC_CAPS AUDIO_CAPS VIDEO_CAPS
+
 GST_DEBUG_CATEGORY_STATIC (gst_agnostic_bin_debug);
 #define GST_CAT_DEFAULT gst_agnostic_bin_debug
 
@@ -31,13 +115,13 @@ enum
 static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("ANY")
+    GST_STATIC_CAPS (AGNOSTIC_CAPS)
     );
 
 static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE ("src_%u",
     GST_PAD_SRC,
     GST_PAD_REQUEST,
-    GST_STATIC_CAPS ("ANY")
+    GST_STATIC_CAPS (AGNOSTIC_CAPS)
     );
 
 static void
@@ -230,8 +314,10 @@ gst_agnostic_bin_class_init (GstAgnosticBinClass * klass)
   gst_element_class_add_pad_template (gstelement_class,
       gst_static_pad_template_get (&sink_factory));
 
-  gstelement_class->request_new_pad = gst_agnostic_bin_request_new_pad;
-  gstelement_class->release_pad = gst_agnostic_bin_release_pad;
+  gstelement_class->request_new_pad =
+      GST_DEBUG_FUNCPTR (gst_agnostic_bin_request_new_pad);
+  gstelement_class->release_pad =
+      GST_DEBUG_FUNCPTR (gst_agnostic_bin_release_pad);
 
   GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, "agnosticbin", 0, "agnosticbin");
 }
@@ -241,6 +327,7 @@ gst_agnostic_bin_init (GstAgnosticBin * agnosticbin)
 {
   GstPad *target_sink;
   GstElement *tee, *queue, *fakesink;
+  GstPadTemplate *templ;
 
   tee = gst_element_factory_make ("tee", NULL);
   queue = gst_element_factory_make ("queue", NULL);
@@ -253,9 +340,13 @@ gst_agnostic_bin_init (GstAgnosticBin * agnosticbin)
       GST_MSECOND * 100, NULL);
 
   target_sink = gst_element_get_static_pad (tee, "sink");
+  templ = gst_static_pad_template_get (&sink_factory);
 
   agnosticbin->sink_caps = NULL;
-  agnosticbin->sinkpad = gst_ghost_pad_new ("sink", target_sink);
+  agnosticbin->sinkpad =
+      gst_ghost_pad_new_from_template ("sink", target_sink, templ);
+
+  g_object_unref (templ);
 
   g_object_unref (target_sink);
 
