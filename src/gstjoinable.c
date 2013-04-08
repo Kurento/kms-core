6dcd7615d211b8bf3bfd9e70831add6dfd035fc0
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-04-08 17:21:08 +0200

    Add basic implementation of Joinable object
    Issue: #130
    Change-Id: I715a6e57e17e8d6cd3bd6e42882268402b55761a
    Reviewed-on: https://repository.kurento.com:9999/2360
    Reviewed-by: Miguel París Díaz <mparisdiaz@gmail.com>
    Tested-by: Miguel París Díaz <mparisdiaz@gmail.com>
    Reviewed-by: Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
    Tested-by: Jose Antonio Santos Cadenas <santoscadenas@gmail.com>

---
+++ src/gstjoinable.c
@@ -0,0 +1,169 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gst/gst.h>
+
+#include "gstjoinable.h"
+#include "gstagnosticbin.h"
+
+#define PLUGIN_NAME "joinable"
+
+GST_DEBUG_CATEGORY_STATIC (gst_joinable_debug);
+#define GST_CAT_DEFAULT gst_joinable_debug
+
+#define gst_joinable_parent_class parent_class
+G_DEFINE_TYPE (GstJoinable, gst_joinable, GST_TYPE_BIN);
+
+#define AUDIO_AGNOSTICBIN "audio_agnosticbin"
+#define VIDEO_AGNOSTICBIN "video_agnosticbin"
+
+/* Signals and args */
+enum
+{
+  LAST_SIGNAL
+};
+
+enum
+{
+  PROP_0
+};
+
+/* the capabilities of the inputs and outputs. */
+static GstStaticPadTemplate audio_sink_factory =
+GST_STATIC_PAD_TEMPLATE ("audio_sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS (AUDIO_CAPS)
+    );
+
+static GstStaticPadTemplate video_sink_factory =
+GST_STATIC_PAD_TEMPLATE ("video_sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS (VIDEO_CAPS)
+    );
+
+static GstStaticPadTemplate audio_src_factory =
+GST_STATIC_PAD_TEMPLATE ("audio_src_%u",
+    GST_PAD_SRC,
+    GST_PAD_REQUEST,
+    GST_STATIC_CAPS (AUDIO_CAPS)
+    );
+
+static GstStaticPadTemplate video_src_factory =
+GST_STATIC_PAD_TEMPLATE ("video_src_%u",
+    GST_PAD_SRC,
+    GST_PAD_REQUEST,
+    GST_STATIC_CAPS (VIDEO_CAPS)
+    );
+
+static GstPad *
+gst_joinable_request_new_pad (GstElement * element,
+    GstPadTemplate * templ, const gchar * name, const GstCaps * caps)
+{
+  GstPad *ret_pad, *agnostic_pad;
+  GstElement *agnosticbin;
+  gchar *pad_name, *pad_name_prefix;
+
+  if (templ ==
+      gst_element_class_get_pad_template (GST_ELEMENT_CLASS (G_OBJECT_GET_CLASS
+              (element)), "audio_src_%u")) {
+    agnosticbin = GST_JOINABLE (element)->audio_agnosticbin;
+    pad_name_prefix = "audio_";
+  } else if (templ ==
+      gst_element_class_get_pad_template (GST_ELEMENT_CLASS (G_OBJECT_GET_CLASS
+              (element)), "video_src_%u")) {
+    agnosticbin = GST_JOINABLE (element)->video_agnosticbin;
+    pad_name_prefix = "video_";
+  } else {
+    return NULL;
+  }
+
+  agnostic_pad = gst_element_get_request_pad (agnosticbin, "src_%u");
+
+  pad_name =
+      g_strdup_printf ("%s%s", pad_name_prefix, GST_OBJECT_NAME (agnostic_pad));
+  ret_pad = gst_ghost_pad_new_from_template (pad_name, agnostic_pad, templ);
+  g_free (pad_name);
+
+  if (ret_pad == NULL)
+    return NULL;
+
+  if (GST_STATE (element) >= GST_STATE_PAUSED
+      || GST_STATE_PENDING (element) >= GST_STATE_PAUSED
+      || GST_STATE_TARGET (element) >= GST_STATE_PAUSED)
+    gst_pad_set_active (ret_pad, TRUE);
+
+  if (gst_element_add_pad (element, ret_pad))
+    return ret_pad;
+
+  g_object_unref (ret_pad);
+  gst_element_release_request_pad (agnosticbin, agnostic_pad);
+
+  return NULL;
+}
+
+static void
+gst_joinable_release_pad (GstElement * element, GstPad * pad)
+{
+  GstElement *agnosticbin;
+  GstPad *target;
+
+  if (g_str_has_prefix ("audio_src", GST_OBJECT_NAME (pad))) {
+    agnosticbin = GST_JOINABLE (element)->audio_agnosticbin;
+  } else if (g_str_has_prefix ("audio_src", GST_OBJECT_NAME (pad))) {
+    agnosticbin = GST_JOINABLE (element)->audio_agnosticbin;
+  } else {
+    return;
+  }
+
+  target = gst_ghost_pad_get_target (GST_GHOST_PAD (pad));
+
+  if (target != NULL && agnosticbin != NULL)
+    gst_element_release_request_pad (agnosticbin, target);
+
+  if (GST_STATE (element) >= GST_STATE_PAUSED
+      || GST_STATE_PENDING (element) >= GST_STATE_PAUSED)
+    gst_pad_set_active (pad, FALSE);
+
+  gst_element_remove_pad (element, pad);
+}
+
+static void
+gst_joinable_class_init (GstJoinableClass * klass)
+{
+  GstElementClass *gstelement_class;
+
+  gstelement_class = GST_ELEMENT_CLASS (klass);
+  gst_element_class_set_details_simple (gstelement_class,
+      "Joinable",
+      "Base/Bin/Joinable",
+      "Base class for joinables",
+      "José Antonio Santos Cadenas <santoscadenas@kurento.com>");
+  gstelement_class->request_new_pad =
+      GST_DEBUG_FUNCPTR (gst_joinable_request_new_pad);
+  gstelement_class->release_pad = GST_DEBUG_FUNCPTR (gst_joinable_release_pad);
+  gst_element_class_add_pad_template (gstelement_class,
+      gst_static_pad_template_get (&audio_src_factory));
+  gst_element_class_add_pad_template (gstelement_class,
+      gst_static_pad_template_get (&video_src_factory));
+  gst_element_class_add_pad_template (gstelement_class,
+      gst_static_pad_template_get (&audio_sink_factory));
+  gst_element_class_add_pad_template (gstelement_class,
+      gst_static_pad_template_get (&video_sink_factory));
+  GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, PLUGIN_NAME, 0, PLUGIN_NAME);
+}
+
+static void
+gst_joinable_init (GstJoinable * joinable)
+{
+  GstElement *audio_agnosticbin, *video_agnosticbin;
+
+  audio_agnosticbin =
+      gst_element_factory_make ("agnosticbin", AUDIO_AGNOSTICBIN);
+  video_agnosticbin =
+      gst_element_factory_make ("agnosticbin", VIDEO_AGNOSTICBIN);
+  gst_bin_add_many (GST_BIN (joinable), audio_agnosticbin,
+      video_agnosticbin, NULL);
+}
---
+++ src/gstjoinable.h
@@ -0,0 +1,46 @@
+#ifndef __GST_JOINABLE_H__
+#define __GST_JOINABLE_H__
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+/* #defines don't like whitespacey bits */
+#define GST_TYPE_JOINABLE \
+  (gst_joinable_get_type())
+#define GST_JOINABLE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_JOINABLE,GstJoinable))
+#define GST_JOINABLE_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_JOINABLE,GstJoinableClass))
+#define GST_IS_JOINABLE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_JOINABLE))
+#define GST_IS_JOINABLE_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_JOINABLE))
+#define GST_JOINABLE_CAST(obj) ((GstJoinable*)(obj))
+typedef struct _GstJoinable GstJoinable;
+typedef struct _GstJoinableClass GstJoinableClass;
+
+#define GST_JOINABLE_LOCK(elem) \
+  (g_rec_mutex_lock (&GST_JOINABLE_CAST ((elem))->media_mutex))
+#define GST_JOINABLE_UNLOCK(elem) \
+  (g_rec_mutex_unlock (&GST_JOINABLE_CAST ((elem))->media_mutex))
+
+struct _GstJoinable
+{
+  GstBin parent;
+
+  GstElement *audio_agnosticbin;
+  GstElement *video_agnosticbin;
+
+  GstPad *audio_sink;
+  GstPad *audio_src;
+};
+
+struct _GstJoinableClass
+{
+  GstBinClass parent_class;
+};
+
+GType gst_joinable_get_type (void);
+
+G_END_DECLS
+#endif /* __GST_JOINABLE_H__ */
