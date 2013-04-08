7c52bf73f8b4d3e34d19f0ae82439e870b7313d0
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-04-08 10:43:37 +0200

    Modify autogen.sh to allow compile in a diferent directory
    If you define BUILD_DIR variable, then autogen will run configure script in that
    diretory and Makefiles will generated in that directory
    
    Change-Id: I189388c5b39f830ee6583cd69e1a9c64f8fe9067
    Reviewed-on: https://repository.kurento.com:9999/2355
    Reviewed-by: Miguel París Díaz <mparisdiaz@gmail.com>
    Tested-by: Miguel París Díaz <mparisdiaz@gmail.com>
    Reviewed-by: Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
    Tested-by: Jose Antonio Santos Cadenas <santoscadenas@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -3,16 +3,30 @@
 # ACLOCAL, AUTOPOINT and/or LIBTOOLIZE to the right versions, or leave them
 # unset and get the defaults
 
+SRC_DIR=`pwd`
+
 autoreconf --verbose --force --install --make || {
  echo 'autogen.sh failed';
  exit 1;
 }
 
-./configure $@ || {
+if [[ -z "$BUILD_DIR" ]]; then
+  BUILD_DIR=$SRC_DIR
+fi
+
+if [ ! -d $BUILD_DIR ]; then
+ mkdir $BUILD_DIR
+fi
+
+cd $BUILD_DIR
+
+$SRC_DIR/configure $@ || {
  echo 'configure failed';
+ cd $SRC_DIR
  exit 1;
 }
 
+cd $SRC_DIR
 # install pre-commit hook for doing clean commits
 if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \);
 then
@@ -20,6 +34,13 @@ rm -f .git/hooks/pre-commit
 ln -s ../../hooks/pre-commit.hook .git/hooks/pre-commit
 fi
 
+if [ $SRC_DIR == $BUILD_DIR ]; then
+ MAKE_STR="make"
+else
+
+ MAKE_STR="cd ${BUILD_DIR#$SRC_DIR}; make"
+fi
+
 echo
-echo "Now type 'make' to compile this module."
+echo "Now type '$MAKE_STR' to compile this module."
 echo
