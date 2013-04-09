e966a5de5b364c935b0468893d919455f2e1beb2
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-04-09 11:28:58 +0200

    Require bash instead of sh in autogen.sh
    Change-Id: Icae860872025898430d95b9076d499c30baf31d6
    Reviewed-on: https://repository.kurento.com/gerrit/2365
    Reviewed-by: Miguel París Díaz <mparisdiaz@gmail.com>
    Tested-by: Miguel París Díaz <mparisdiaz@gmail.com>
    Reviewed-by: Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
    Tested-by: Jose Antonio Santos Cadenas <santoscadenas@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # you can either set the environment variables AUTOCONF, AUTOHEADER, AUTOMAKE,
 # ACLOCAL, AUTOPOINT and/or LIBTOOLIZE to the right versions, or leave them
 # unset and get the defaults
