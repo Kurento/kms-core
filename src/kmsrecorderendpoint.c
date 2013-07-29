785b033b9ca7f0c5a007f5a47b52561701ddf12e
Santiago Carot-Nemesio <sancane@gmail.com>
2013-07-29 15:37:30 +0200

    Get rid of function prototype declarations
    Change-Id: Ib9439fad9bea16d90623cee13d1d5da53bef6630

--- src/kmsrecorderendpoint.c
+++ src/kmsrecorderendpoint.c
@@ -23,11 +23,6 @@ struct _KmsRecorderEndPointPrivate
   gchar *uri;
 };
 
-/* prototypes */
-
-static void kms_recorder_end_point_dispose (GObject * object);
-static void kms_recorder_end_point_finalize (GObject * object);
-
 enum
 {
   PROP_0,
@@ -46,7 +41,7 @@ G_DEFINE_TYPE_WITH_CODE (KmsRecorderEndPoint, kms_recorder_end_point,
     GST_DEBUG_CATEGORY_INIT (kms_recorder_end_point_debug_category, PLUGIN_NAME,
         0, "debug category for recorderendpoint element"));
 
-void
+static void
 kms_recorder_end_point_set_property (GObject * object, guint property_id,
     const GValue * value, GParamSpec * pspec)
 {
@@ -85,6 +80,30 @@ kms_recorder_end_point_get_property (GObject * object, guint property_id,
   }
 }
 
+void
+kms_recorder_end_point_dispose (GObject * object)
+{
+  KmsRecorderEndPoint *recorderendpoint = KMS_RECORDER_END_POINT (object);
+
+  GST_DEBUG_OBJECT (recorderendpoint, "dispose");
+
+  /* clean up as possible.  may be called multiple times */
+
+  G_OBJECT_CLASS (kms_recorder_end_point_parent_class)->dispose (object);
+}
+
+void
+kms_recorder_end_point_finalize (GObject * object)
+{
+  KmsRecorderEndPoint *recorderendpoint = KMS_RECORDER_END_POINT (object);
+
+  GST_DEBUG_OBJECT (recorderendpoint, "finalize");
+
+  /* clean up object here */
+
+  G_OBJECT_CLASS (kms_recorder_end_point_parent_class)->finalize (object);
+}
+
 static void
 kms_recorder_end_point_class_init (KmsRecorderEndPointClass * klass)
 {
@@ -115,30 +134,6 @@ static void
 kms_recorder_end_point_init (KmsRecorderEndPoint * self)
 {
   self->priv = KMS_RECORDER_END_POINT_GET_PRIVATE (self);
-}
-
-void
-kms_recorder_end_point_dispose (GObject * object)
-{
-  KmsRecorderEndPoint *recorderendpoint = KMS_RECORDER_END_POINT (object);
-
-  GST_DEBUG_OBJECT (recorderendpoint, "dispose");
-
-  /* clean up as possible.  may be called multiple times */
-
-  G_OBJECT_CLASS (kms_recorder_end_point_parent_class)->dispose (object);
-}
-
-void
-kms_recorder_end_point_finalize (GObject * object)
-{
-  KmsRecorderEndPoint *recorderendpoint = KMS_RECORDER_END_POINT (object);
-
-  GST_DEBUG_OBJECT (recorderendpoint, "finalize");
-
-  /* clean up object here */
-
-  G_OBJECT_CLASS (kms_recorder_end_point_parent_class)->finalize (object);
 }
 
 gboolean
