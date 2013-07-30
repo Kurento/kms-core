bcb38f1b860717428cb30c44a1089ccfba884e83
Santiago Carot-Nemesio <sancane@gmail.com>
2013-07-30 09:17:48 +0200

    Set multiline macros to prevent them from being longer than 80 characteres
    Change-Id: I32c83b0e1d0ec18bda3fca342236998b3f0c808c

--- src/kmsrecorderendpoint.h
+++ src/kmsrecorderendpoint.h
@@ -2,11 +2,30 @@
 #define _KMS_RECORDER_END_POINT_H_
 
 G_BEGIN_DECLS
-#define KMS_TYPE_RECORDER_END_POINT   (kms_recorder_end_point_get_type())
-#define KMS_RECORDER_END_POINT(obj)   (G_TYPE_CHECK_INSTANCE_CAST((obj),KMS_TYPE_RECORDER_END_POINT,KmsRecorderEndPoint))
-#define KMS_RECORDER_END_POINT_CLASS(klass)   (G_TYPE_CHECK_CLASS_CAST((klass),KMS_TYPE_RECORDER_END_POINT,KmsRecorderEndPointClass))
-#define KMS_IS_RECORDER_END_POINT(obj)   (G_TYPE_CHECK_INSTANCE_TYPE((obj),KMS_TYPE_RECORDER_END_POINT))
-#define KMS_IS_RECORDER_END_POINT_CLASS(obj)   (G_TYPE_CHECK_CLASS_TYPE((klass),KMS_TYPE_RECORDER_END_POINT))
+#define KMS_TYPE_RECORDER_END_POINT \
+  (kms_recorder_end_point_get_type())
+#define KMS_RECORDER_END_POINT(obj) (          \
+  G_TYPE_CHECK_INSTANCE_CAST(                  \
+    (obj),                                     \
+    KMS_TYPE_RECORDER_END_POINT,               \
+    KmsRecorderEndPoint                        \
+  )                                            \
+)
+#define KMS_RECORDER_END_POINT_CLASS(klass) (  \
+  G_TYPE_CHECK_CLASS_CAST (                    \
+    (klass),                                   \
+    KMS_TYPE_RECORDER_END_POINT,               \
+    KmsRecorderEndPointClass                   \
+  )                                            \
+)
+#define KMS_IS_RECORDER_END_POINT(obj) (       \
+  G_TYPE_CHECK_INSTANCE_TYPE (                 \
+    (obj),                                     \
+    KMS_TYPE_RECORDER_END_POINT                \
+  )                                            \
+)
+#define KMS_IS_RECORDER_END_POINT_CLASS(obj) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),KMS_TYPE_RECORDER_END_POINT))
 typedef struct _KmsRecorderEndPoint KmsRecorderEndPoint;
 typedef struct _KmsRecorderEndPointClass KmsRecorderEndPointClass;
 typedef struct _KmsRecorderEndPointPrivate KmsRecorderEndPointPrivate;
