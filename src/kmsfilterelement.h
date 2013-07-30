36ef2586baf33aa0d1f4a80c59f766503b19b6c1
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-07-30 16:11:08 +0200

    Avoid too long lines with multiline macros
    Change-Id: Ic0b541fff21aac4ce5ec6d0181afd00af447f63c

--- src/kmsfilterelement.h
+++ src/kmsfilterelement.h
@@ -2,11 +2,33 @@
 #define _KMS_FILTER_ELEMENT_H_
 
 G_BEGIN_DECLS
-#define KMS_TYPE_FILTER_ELEMENT   (kms_filter_element_get_type())
-#define KMS_FILTER_ELEMENT(obj)   (G_TYPE_CHECK_INSTANCE_CAST((obj),KMS_TYPE_FILTER_ELEMENT,KmsFilterElement))
-#define KMS_FILTER_ELEMENT_CLASS(klass)   (G_TYPE_CHECK_CLASS_CAST((klass),KMS_TYPE_FILTER_ELEMENT,KmsFilterElementClass))
-#define KMS_IS_FILTER_ELEMENT(obj)   (G_TYPE_CHECK_INSTANCE_TYPE((obj),KMS_TYPE_FILTER_ELEMENT))
-#define KMS_IS_FILTER_ELEMENT_CLASS(obj)   (G_TYPE_CHECK_CLASS_TYPE((klass),KMS_TYPE_FILTER_ELEMENT))
+#define KMS_TYPE_FILTER_ELEMENT (kms_filter_element_get_type())
+#define KMS_FILTER_ELEMENT(obj) (               \
+  G_TYPE_CHECK_INSTANCE_CAST (                  \
+    (obj),                                      \
+    KMS_TYPE_FILTER_ELEMENT,                    \
+    KmsFilterElement                            \
+  )                                             \
+)
+#define KMS_FILTER_ELEMENT_CLASS(klass) (       \
+  G_TYPE_CHECK_CLASS_CAST (                     \
+    (klass),                                    \
+    KMS_TYPE_FILTER_ELEMENT,                    \
+    KmsFilterElementClass                       \
+  )                                             \
+)
+#define KMS_IS_FILTER_ELEMENT(obj) (            \
+  G_TYPE_CHECK_INSTANCE_TYPE (                  \
+    (obj),                                      \
+    KMS_TYPE_FILTER_ELEMENT                     \
+    )                                           \
+)
+#define KMS_IS_FILTER_ELEMENT_CLASS(obj) (      \
+  G_TYPE_CHECK_CLASS_TYPE (                     \
+  (klass),                                      \
+  KMS_TYPE_FILTER_ELEMENT                       \
+  )                                             \
+)
 typedef struct _KmsFilterElement KmsFilterElement;
 typedef struct _KmsFilterElementClass KmsFilterElementClass;
 
