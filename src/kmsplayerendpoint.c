50bf24f43bf7999e158fbf27f3234916d1bebdad
Joaquin Mengual Garcia <kini.mengual@gmail.com>
2013-07-31 12:17:59 +0200

    Change parent class of playerendpoint
    Change-Id: I7fd559775062dd3fa420ed63797e1823d0be0159

--- src/kmsplayerendpoint.c
+++ src/kmsplayerendpoint.c
@@ -19,16 +19,8 @@ GST_DEBUG_CATEGORY_STATIC (kms_player_end_point_debug_category);
   )                                             \
 )
 
-struct _KmsPlayerEndPointPrivate
+struct _KmsRecorderEndPointPrivate
 {
-  gchar *uri;
-};
-
-/* properties */
-enum
-{
-  PROP_0,
-  PROP_URI
 };
 
 /* pad templates */
@@ -36,49 +28,11 @@ enum
 /* class initialization */
 
 G_DEFINE_TYPE_WITH_CODE (KmsPlayerEndPoint, kms_player_end_point,
-    KMS_TYPE_ELEMENT,
+    KMS_TYPE_URI_END_POINT,
     GST_DEBUG_CATEGORY_INIT (kms_player_end_point_debug_category, PLUGIN_NAME,
         0, "debug category for playerendpoint element"));
 
 static void
-kms_player_end_point_get_property (GObject * object, guint prop_id,
-    GValue * value, GParamSpec * pspec)
-{
-  KmsPlayerEndPoint *self = KMS_PLAYER_END_POINT (object);
-
-  GST_DEBUG_OBJECT (self, "get_property");
-
-  switch (prop_id) {
-    case PROP_URI:
-      g_value_set_string (value, self->priv->uri);
-      break;
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-      break;
-  }
-}
-
-static void
-kms_player_end_point_set_property (GObject * object, guint prop_id,
-    const GValue * value, GParamSpec * pspec)
-{
-  KmsPlayerEndPoint *self = KMS_PLAYER_END_POINT (object);
-
-  GST_DEBUG_OBJECT (self, "set_property");
-
-  switch (prop_id) {
-    case PROP_URI:
-      if (self->priv->uri != NULL)
-        g_free (self->priv->uri);
-      self->priv->uri = g_value_dup_string (value);
-      break;
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-      break;
-  }
-}
-
-void
 kms_player_end_point_dispose (GObject * object)
 {
   KmsPlayerEndPoint *playerendpoint = KMS_PLAYER_END_POINT (object);
@@ -90,7 +44,7 @@ kms_player_end_point_dispose (GObject * object)
   G_OBJECT_CLASS (kms_player_end_point_parent_class)->dispose (object);
 }
 
-void
+static void
 kms_player_end_point_finalize (GObject * object)
 {
   KmsPlayerEndPoint *playerendpoint = KMS_PLAYER_END_POINT (object);
@@ -113,18 +67,6 @@ kms_player_end_point_class_init (KmsPlayerEndPointClass * klass)
 
   gobject_class->dispose = kms_player_end_point_dispose;
   gobject_class->finalize = kms_player_end_point_finalize;
-
-  /* define virtual function pointers */
-  gobject_class->set_property = kms_player_end_point_set_property;
-  gobject_class->get_property = kms_player_end_point_get_property;
-
-  /* define properties */
-  g_object_class_install_property (gobject_class, PROP_URI,
-      g_param_spec_string ("uri", "uri", "URI of the resource to play", NULL,
-          G_PARAM_READABLE | G_PARAM_WRITABLE));
-  /* Registers a private structure for the instantiatable type */
-  g_type_class_add_private (klass, sizeof (KmsPlayerEndPointPrivate));
-
 }
 
 static void
--- src/kmsplayerendpoint.h
+++ src/kmsplayerendpoint.h
@@ -1,19 +1,32 @@
 #ifndef _KMS_PLAYER_END_POINT_H_
 #define _KMS_PLAYER_END_POINT_H_
 
+#include "kmsuriendpoint.h"
+
 G_BEGIN_DECLS
-#define KMS_TYPE_PLAYER_END_POINT   (kms_player_end_point_get_type())
-#define KMS_PLAYER_END_POINT(obj)   (G_TYPE_CHECK_INSTANCE_CAST((obj),KMS_TYPE_PLAYER_END_POINT,KmsPlayerEndPoint))
-#define KMS_PLAYER_END_POINT_CLASS(klass)   (G_TYPE_CHECK_CLASS_CAST((klass),KMS_TYPE_PLAYER_END_POINT,KmsPlayerEndPointClass))
-#define KMS_IS_PLAYER_END_POINT(obj)   (G_TYPE_CHECK_INSTANCE_TYPE((obj),KMS_TYPE_PLAYER_END_POINT))
-#define KMS_IS_PLAYER_END_POINT_CLASS(obj)   (G_TYPE_CHECK_CLASS_TYPE((klass),KMS_TYPE_PLAYER_END_POINT))
+#define KMS_TYPE_PLAYER_END_POINT               \
+  (kms_player_end_point_get_type())
+#define KMS_PLAYER_END_POINT(obj)               \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),           \
+  KMS_TYPE_PLAYER_END_POINT,KmsPlayerEndPoint))
+#define KMS_PLAYER_END_POINT_CLASS(klass)       \
+  (G_TYPE_CHECK_CLASS_CAST((klass),             \
+  KMS_TYPE_PLAYER_END_POINT,                    \
+  KmsPlayerEndPointClass))
+#define KMS_IS_PLAYER_END_POINT(obj)            \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),            \
+  KMS_TYPE_PLAYER_END_POINT))
+#define KMS_IS_PLAYER_END_POINT_CLASS(klass)    \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),             \
+  KMS_TYPE_PLAYER_END_POINT))
+
 typedef struct _KmsPlayerEndPoint KmsPlayerEndPoint;
 typedef struct _KmsPlayerEndPointClass KmsPlayerEndPointClass;
 typedef struct _KmsPlayerEndPointPrivate KmsPlayerEndPointPrivate;
 
 struct _KmsPlayerEndPoint
 {
-  KmsElement parent;
+  KmsUriEndPoint parent;
 
   /*< private > */
   KmsPlayerEndPointPrivate *priv;
@@ -21,7 +34,7 @@ struct _KmsPlayerEndPoint
 
 struct _KmsPlayerEndPointClass
 {
-  KmsElementClass parent_class;
+  KmsUriEndPointClass parent_class;
 };
 
 GType kms_player_end_point_get_type (void);
