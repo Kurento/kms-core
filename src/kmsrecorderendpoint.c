024593bdd0e597a410a27c42374b856e4623a1e6
Santiago Carot-Nemesio <sancane@gmail.com>
2013-08-02 09:58:25 +0200

    Insert buffers from recorderendpoint's sinkapp to the internal pipeline
    Change-Id: I907304545cdb2fa8f1f92baf58cd5873fd6589d1

--- src/kmsrecorderendpoint.c
+++ src/kmsrecorderendpoint.c
@@ -6,6 +6,7 @@
 #include <gst/app/gstappsrc.h>
 #include <gst/app/gstappsink.h>
 
+#include "kmsagnosticcaps.h"
 #include "kmsrecorderendpoint.h"
 
 #define PLUGIN_NAME "recorderendpoint"
@@ -29,6 +30,7 @@ GST_DEBUG_CATEGORY_STATIC (kms_recorder_end_point_debug_category);
 struct _KmsRecorderEndPointPrivate
 {
   GstElement *pipeline;
+  guint count;
 };
 
 /* class initialization */
@@ -69,49 +71,171 @@ kms_recorder_end_point_finalize (GObject * object)
 }
 
 static void
-recv_audio_sample (GstAppSink * gstappsink, gpointer user_data)
+recv_sample (GstAppSink * appsink, gpointer user_data)
 {
   GstElement *appsrc = GST_ELEMENT (user_data);
+  GstFlowReturn ret;
+  GstSample *sample;
+  GstBuffer *buffer;
+  GstCaps *caps;
 
-  GST_DEBUG ("TODO: Push audio buffer to %p", GST_ELEMENT_NAME (appsrc));
+  g_object_get (G_OBJECT (appsink), "last-sample", &sample, NULL);
+  if (sample == NULL)
+    return;
+
+  g_object_get (G_OBJECT (appsrc), "caps", &caps, NULL);
+  if (caps == NULL) {
+    /* Appsrc has not yet caps defined */
+    caps = gst_sample_get_caps (sample);
+    if (caps != NULL)
+      g_object_set (appsrc, "caps", caps, NULL);
+    else
+      GST_ERROR ("No caps found for %s", GST_ELEMENT_NAME (appsrc));
+  }
+
+  buffer = gst_sample_get_buffer (sample);
+  if (buffer == NULL)
+    return;
+
+  g_signal_emit_by_name (appsrc, "push-buffer", buffer, &ret);
+
+  if (ret != GST_FLOW_OK) {
+    /* something wrong */
+    GST_ERROR ("Could not send audio buffer to appsrc. Ret code %d", ret);
+  }
+}
+
+static GstElement *
+kms_recorder_end_point_get_sink (KmsRecorderEndPoint * self)
+{
+  /* TODO: Get proper sink element based on the uri */
+  GstElement *filesink = gst_element_factory_make ("filesink", NULL);
+  gchar *filename;
+
+  /* TODO: lock mutex */
+  filename =
+      g_strdup_printf ("%d_%s", self->priv->count++,
+      KMS_URI_END_POINT (self)->uri);
+  /* TODO: unlock mutex */
+
+  g_object_set (G_OBJECT (filesink), "location", filename, NULL);
+  g_free (filename);
+
+  return filesink;
 }
 
 static void
-recv_video_sample (GstAppSink * gstappsink, gpointer user_data)
+pad_added (GstElement * element, GstPad * pad, gpointer data)
 {
-  GstElement *appsrc = GST_ELEMENT (user_data);
+  KmsRecorderEndPoint *self = KMS_RECORDER_END_POINT (data);
+  GstElement *sink;
+  GstPad *sinkpad;
 
-  GST_DEBUG ("TODO: Push video buffer to %p", GST_ELEMENT_NAME (appsrc));
-}
+  GST_INFO ("Pad added");
 
-static void
-pad_added (GstElement * element, GstPad * pad)
-{
-  GST_DEBUG ("TODO: Pad added");
+  if (GST_PAD_IS_SINK (pad))
+    return;
+
+  sink = kms_recorder_end_point_get_sink (self);
+
+  gst_bin_add (GST_BIN (self->priv->pipeline), sink);
+  gst_element_sync_state_with_parent (sink);
+
+  sinkpad = gst_element_get_static_pad (sink, "sink");
+
+  if (gst_pad_link (pad, sinkpad) != GST_PAD_LINK_OK)
+    GST_ERROR ("Could not link %s with %s", GST_ELEMENT_NAME (element),
+        GST_ELEMENT_NAME (sink));
+
+  gst_object_unref (sinkpad);
 }
 
 static void
 pad_removed (GstElement * element, GstPad * pad, gpointer data)
 {
-  GST_DEBUG ("TODO: Pad removed");
+  KmsRecorderEndPoint *self = KMS_RECORDER_END_POINT (data);
+  GstElement *sink;
+  GstPad *peer;
+
+  GST_INFO ("Pad removed");
+
+  if (GST_PAD_IS_SINK (pad))
+    return;
+
+  peer = gst_pad_get_peer (pad);
+  if (peer == NULL)
+    return;
+
+  sink = gst_pad_get_parent_element (peer);
+  if (sink == NULL) {
+    GST_ERROR ("No parent element for pad %s was faound",
+        GST_ELEMENT_NAME (sink));
+    return;
+  }
+
+  gst_pad_unlink (pad, peer);
+
+  if (!gst_element_set_locked_state (sink, TRUE))
+    GST_ERROR ("Could not block element %s", GST_ELEMENT_NAME (sink));
+
+  GST_DEBUG ("Removing sink %s from %s", GST_ELEMENT_NAME (sink),
+      GST_ELEMENT_NAME (self->priv->pipeline));
+
+  gst_element_set_state (sink, GST_STATE_NULL);
+  gst_bin_remove (GST_BIN (self->priv->pipeline), sink);
+
+  gst_object_unref (peer);
+  g_object_unref (sink);
 }
 
 static void
-kms_recorder_end_point_stopped (KmsUriEndPoint * self)
+kms_recorder_end_point_open_valves (KmsRecorderEndPoint * self)
 {
-  GST_DEBUG ("TODO: Implement stopped");
+  g_object_set (KMS_ELEMENT (self)->audio_valve, "drop", FALSE, NULL);
+  g_object_set (KMS_ELEMENT (self)->video_valve, "drop", FALSE, NULL);
 }
 
 static void
-kms_recorder_end_point_started (KmsUriEndPoint * self)
+kms_recorder_end_point_close_valves (KmsRecorderEndPoint * self)
 {
-  GST_DEBUG ("TODO: Implement started");
+  g_object_set (KMS_ELEMENT (self)->audio_valve, "drop", TRUE, NULL);
+  g_object_set (KMS_ELEMENT (self)->video_valve, "drop", TRUE, NULL);
 }
 
 static void
-kms_recorder_end_point_paused (KmsUriEndPoint * self)
+kms_recorder_end_point_stopped (KmsUriEndPoint * obj)
 {
-  GST_DEBUG ("TODO: Implement paused");
+  KmsRecorderEndPoint *self = KMS_RECORDER_END_POINT (obj);
+
+  /* Close valves */
+  kms_recorder_end_point_close_valves (self);
+
+  /* Set internal pipeline to NULL */
+  gst_element_set_state (self->priv->pipeline, GST_STATE_NULL);
+}
+
+static void
+kms_recorder_end_point_started (KmsUriEndPoint * obj)
+{
+  KmsRecorderEndPoint *self = KMS_RECORDER_END_POINT (obj);
+
+  /* Set internal pipeline to playing */
+  gst_element_set_state (self->priv->pipeline, GST_STATE_PLAYING);
+
+  /* Open valves */
+  kms_recorder_end_point_open_valves (self);
+}
+
+static void
+kms_recorder_end_point_paused (KmsUriEndPoint * obj)
+{
+  KmsRecorderEndPoint *self = KMS_RECORDER_END_POINT (obj);
+
+  /* Close valves */
+  kms_recorder_end_point_close_valves (self);
+
+  /* Set internal pipeline to GST_STATE_PAUSED */
+  gst_element_set_state (self->priv->pipeline, GST_STATE_PAUSED);
 }
 
 static void
@@ -139,11 +263,21 @@ static void
 kms_recorder_end_point_init (KmsRecorderEndPoint * self)
 {
   GstElement *audiosink, *audiosrc, *videosink, *videosrc, *automuxer;
+  GstCaps *audio_caps = gst_caps_from_string (KMS_AGNOSTIC_AUDIO_CAPS);
+  GstCaps *video_caps = gst_caps_from_string (KMS_AGNOSTIC_VIDEO_CAPS);
 
   self->priv = KMS_RECORDER_END_POINT_GET_PRIVATE (self);
 
   audiosink = gst_element_factory_make ("appsink", AUDIO_APPSINK);
   videosink = gst_element_factory_make ("appsink", VIDEO_APPSINK);
+
+  g_object_set (audiosink, "emit-signals", TRUE, NULL);
+  g_object_set (audiosink, "caps", audio_caps, NULL);
+  g_object_set (videosink, "emit-signals", TRUE, NULL);
+  g_object_set (videosink, "caps", video_caps, NULL);
+
+  gst_caps_unref (audio_caps);
+  gst_caps_unref (video_caps);
 
   gst_bin_add_many (GST_BIN (self), audiosink, videosink, NULL);
 
@@ -157,6 +291,12 @@ kms_recorder_end_point_init (KmsRecorderEndPoint * self)
   videosrc = gst_element_factory_make ("appsrc", VIDEO_APPSRC);
   automuxer = gst_element_factory_make ("automuxerbin", AUTOMUXER);
 
+  /* setup appsrc */
+  g_object_set (G_OBJECT (audiosrc), "is-live", TRUE, "do-timestamp", TRUE,
+      "min-latency", (gint64) 0, "format", GST_FORMAT_TIME, NULL);
+  g_object_set (G_OBJECT (videosrc), "is-live", TRUE, "do-timestamp", TRUE,
+      "min-latency", (gint64) 0, "format", GST_FORMAT_TIME, NULL);
+
   gst_bin_add_many (GST_BIN (self->priv->pipeline), audiosrc, videosrc,
       automuxer, NULL);
 
@@ -165,18 +305,17 @@ kms_recorder_end_point_init (KmsRecorderEndPoint * self)
   gst_element_link_pads (videosrc, "src", automuxer, "video_%u");
 
   /* Connect to signals */
-  g_signal_connect (audiosink, "new-sample", G_CALLBACK (recv_audio_sample),
+  g_signal_connect (audiosink, "new-sample", G_CALLBACK (recv_sample),
       audiosrc);
-  g_signal_connect (videosink, "new-sample", G_CALLBACK (recv_video_sample),
+  g_signal_connect (videosink, "new-sample", G_CALLBACK (recv_sample),
       videosrc);
-  g_signal_connect (automuxer, "pad-added", G_CALLBACK (pad_added), NULL);
-  g_signal_connect (automuxer, "pad-removed", G_CALLBACK (pad_removed), NULL);
+  g_signal_connect (automuxer, "pad-added", G_CALLBACK (pad_added), self);
+  g_signal_connect (automuxer, "pad-removed", G_CALLBACK (pad_removed), self);
 }
 
 gboolean
 kms_recorder_end_point_plugin_init (GstPlugin * plugin)
 {
-
   return gst_element_register (plugin, PLUGIN_NAME, GST_RANK_NONE,
       KMS_TYPE_RECORDER_END_POINT);
 }
--- tests/check/element/recorderendpoint.c
+++ tests/check/element/recorderendpoint.c
@@ -2,13 +2,105 @@
 #include <gst/gst.h>
 #include <glib.h>
 
-static Suite *
+#include "kmsuriendpointstate.h"
+
+static void
+bus_msg (GstBus * bus, GstMessage * msg, gpointer pipe)
+{
+
+  switch (msg->type) {
+    case GST_MESSAGE_ERROR:{
+      GST_ERROR ("Error: %P", msg);
+      GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipe),
+          GST_DEBUG_GRAPH_SHOW_ALL, "bus_error");
+      fail ("Error received on bus");
+      break;
+    }
+    case GST_MESSAGE_WARNING:{
+      GST_WARNING ("Warning: %P", msg);
+      GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipe),
+          GST_DEBUG_GRAPH_SHOW_ALL, "warning");
+      break;
+    }
+    case GST_MESSAGE_STATE_CHANGED:{
+      GST_INFO ("Event: %P", msg);
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+static gboolean
+timer (gpointer * data)
+{
+  GMainLoop *loop = (GMainLoop *) data;
+
+  g_main_loop_quit (loop);
+
+  return FALSE;
+}
+
+GST_START_TEST (check_pipeline)
+{
+  GstElement *pipeline, *videotestsrc, *audiotestsrc, *recorder;
+  guint bus_watch_id;
+  GMainLoop *loop;
+  GstBus *bus;
+
+  loop = g_main_loop_new (NULL, FALSE);
+
+  /* Create gstreamer elements */
+  pipeline = gst_pipeline_new ("recorderendpoint-test");
+  videotestsrc = gst_element_factory_make ("videotestsrc", NULL);
+  audiotestsrc = gst_element_factory_make ("audiotestsrc", NULL);
+  recorder = gst_element_factory_make ("recorderendpoint", NULL);
+
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+
+  bus_watch_id = gst_bus_add_watch (bus, gst_bus_async_signal_func, NULL);
+  g_signal_connect (bus, "message", G_CALLBACK (bus_msg), pipeline);
+  g_object_unref (bus);
+
+  gst_bin_add_many (GST_BIN (pipeline), audiotestsrc, videotestsrc, recorder,
+      NULL);
+  gst_element_link_pads (videotestsrc, "src", recorder, "video_sink");
+  gst_element_link_pads (audiotestsrc, "src", recorder, "audio_sink");
+
+  g_object_set (G_OBJECT (videotestsrc), "num-buffers", 400, NULL);
+  g_object_set (G_OBJECT (audiotestsrc), "num-buffers", 400, NULL);
+  g_object_set (G_OBJECT (recorder), "uri", "recorder_test.avi", NULL);
+
+  GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
+      GST_DEBUG_GRAPH_SHOW_ALL, "before entering main loop");
+
+  g_timeout_add (5000, (GSourceFunc) timer, loop);
+
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  /* Set recorder to start state */
+  g_object_set (G_OBJECT (recorder), "state", KMS_URI_END_POINT_STATE_START,
+      NULL);
+
+  g_main_loop_run (loop);
+
+  GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
+      GST_DEBUG_GRAPH_SHOW_ALL, "after entering main loop");
+
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (GST_OBJECT (pipeline));
+  g_source_remove (bus_watch_id);
+  g_main_loop_unref (loop);
+}
+
+GST_END_TEST static Suite *
 recorderendpoint_suite (void)
 {
   Suite *s = suite_create ("recorderendpoint");
   TCase *tc_chain = tcase_create ("element");
 
   suite_add_tcase (s, tc_chain);
+  tcase_add_test (tc_chain, check_pipeline);
 
   return s;
 }
