4541825fb7c25a162735fbe838b7ad3c7bef4cd4
Santiago Carot-Nemesio <sancane@gmail.com>
2013-08-05 09:40:59 +0200

    Test state changes in recorderendpoint
    Change-Id: I8c8625f0926a8a3172b02cdd36d555c863c3f702

--- tests/check/element/recorderendpoint.c
+++ tests/check/element/recorderendpoint.c
@@ -4,6 +4,53 @@
 
 #include "kmsuriendpointstate.h"
 
+gboolean set_state_start (gpointer *);
+gboolean set_state_pause (gpointer *);
+gboolean set_state_stop (gpointer *);
+
+static GMainLoop *loop = NULL;
+static GstElement *recorder = NULL;
+static guint state = 0;
+
+struct state_controller
+{
+  KmsUriEndPointState state;
+  guint seconds;
+};
+
+static const struct state_controller trasnsitions[] = {
+  {KMS_URI_END_POINT_STATE_START, 2},
+  {KMS_URI_END_POINT_STATE_START, 3},
+  {KMS_URI_END_POINT_STATE_PAUSE, 2},
+  {KMS_URI_END_POINT_STATE_START, 2},
+  {KMS_URI_END_POINT_STATE_STOP, 3},
+  {KMS_URI_END_POINT_STATE_START, 2},
+  {KMS_URI_END_POINT_STATE_STOP, 2},
+  {KMS_URI_END_POINT_STATE_START, 2}
+};
+
+static gchar *
+state2string (KmsUriEndPointState state)
+{
+  switch (state) {
+    case KMS_URI_END_POINT_STATE_STOP:
+      return "STOP";
+    case KMS_URI_END_POINT_STATE_START:
+      return "START";
+    case KMS_URI_END_POINT_STATE_PAUSE:
+      return "PAUSE";
+    default:
+      return "Invalid state";
+  }
+}
+
+static void
+change_state (KmsUriEndPointState state)
+{
+  GST_DEBUG ("Setting recorder to state %s", state2string (state));
+  g_object_set (G_OBJECT (recorder), "state", state, NULL);
+}
+
 static void
 bus_msg (GstBus * bus, GstMessage * msg, gpointer pipe)
 {
@@ -31,28 +78,40 @@ bus_msg (GstBus * bus, GstMessage * msg, gpointer pipe)
   }
 }
 
-static gboolean
-timer (gpointer * data)
+static gboolean transite_cb (gpointer);
+
+static void
+transite ()
 {
-  GMainLoop *loop = (GMainLoop *) data;
+  if (state < G_N_ELEMENTS (trasnsitions)) {
+    change_state (trasnsitions[state].state);
+    g_timeout_add (trasnsitions[state].seconds * 1000, transite_cb, NULL);
+  } else {
+    GST_DEBUG ("All transitions done. Finishing recorder test suit");
+    g_main_loop_quit (loop);
+  }
+}
 
-  g_main_loop_quit (loop);
-
+static gboolean
+transite_cb (gpointer data)
+{
+  state++;
+  transite ();
   return FALSE;
 }
 
-GST_START_TEST (check_pipeline)
+GST_START_TEST (check_states_pipeline)
 {
-  GstElement *pipeline, *videotestsrc, *audiotestsrc, *recorder;
+  GstElement *pipeline, *videotestsrc, *audiotestsrc, *timeoverlay;
   guint bus_watch_id;
-  GMainLoop *loop;
   GstBus *bus;
 
   loop = g_main_loop_new (NULL, FALSE);
 
   /* Create gstreamer elements */
-  pipeline = gst_pipeline_new ("recorderendpoint-test");
+  pipeline = gst_pipeline_new ("recorderendpoint0-test");
   videotestsrc = gst_element_factory_make ("videotestsrc", NULL);
+  timeoverlay = gst_element_factory_make ("timeoverlay", NULL);
   audiotestsrc = gst_element_factory_make ("audiotestsrc", NULL);
   recorder = gst_element_factory_make ("recorderendpoint", NULL);
 
@@ -63,25 +122,25 @@ GST_START_TEST (check_pipeline)
   g_object_unref (bus);
 
   gst_bin_add_many (GST_BIN (pipeline), audiotestsrc, videotestsrc, recorder,
-      NULL);
-  gst_element_link_pads (videotestsrc, "src", recorder, "video_sink");
+      timeoverlay, NULL);
+  gst_element_link (videotestsrc, timeoverlay);
+  gst_element_link_pads (timeoverlay, "src", recorder, "video_sink");
   gst_element_link_pads (audiotestsrc, "src", recorder, "audio_sink");
 
-  g_object_set (G_OBJECT (videotestsrc), "num-buffers", 400, NULL);
-  g_object_set (G_OBJECT (audiotestsrc), "num-buffers", 400, NULL);
-  g_object_set (G_OBJECT (recorder), "uri", "file:///tmp/recorder_%u.avi",
+  g_object_set (G_OBJECT (recorder), "uri", "file:///tmp/state_recorder_%u.avi",
       NULL);
+  g_object_set (G_OBJECT (videotestsrc), "is-live", TRUE, "do-timestamp", TRUE,
+      "pattern", 18, NULL);
+  g_object_set (G_OBJECT (audiotestsrc), "is-live", TRUE, "do-timestamp", TRUE,
+      NULL);
+  g_object_set (G_OBJECT (timeoverlay), "font-desc", "Sans 28", NULL);
 
   GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
       GST_DEBUG_GRAPH_SHOW_ALL, "before entering main loop");
 
-  g_timeout_add (5000, (GSourceFunc) timer, loop);
-
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
-  /* Set recorder to start state */
-  g_object_set (G_OBJECT (recorder), "state", KMS_URI_END_POINT_STATE_START,
-      NULL);
+  transite ();
 
   g_main_loop_run (loop);
 
@@ -94,15 +153,18 @@ GST_START_TEST (check_pipeline)
   g_main_loop_unref (loop);
 }
 
-GST_END_TEST static Suite *
+GST_END_TEST
+/******************************/
+/* RecorderEndPoint test suit */
+/******************************/
+static Suite *
 recorderendpoint_suite (void)
 {
   Suite *s = suite_create ("recorderendpoint");
   TCase *tc_chain = tcase_create ("element");
 
   suite_add_tcase (s, tc_chain);
-  tcase_add_test (tc_chain, check_pipeline);
-
+  tcase_add_test (tc_chain, check_states_pipeline);
   return s;
 }
 
