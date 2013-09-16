1d8bf71490c5dd8f2b8320748efb25e7cee589d2
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-09-16 11:33:49 +0200

    Remove malformed agnosticbin test
    Change-Id: Id124fe74bd6173e4553da794c18feb174126eb1c

--- tests/check/element/agnosticbin.c
+++ tests/check/element/agnosticbin.c
@@ -51,19 +51,6 @@ bus_msg (GstBus * bus, GstMessage * msg, gpointer pipe)
 }
 
 static void
-negotiated_fakesink_hand_off (GstElement * fakesink, GstBuffer * buf,
-    GstPad * pad, gpointer data)
-{
-  static int count = 0;
-  GMainLoop *loop = (GMainLoop *) data;
-
-  if (count++ > 10) {
-    g_object_set (G_OBJECT (fakesink), "signal-handoffs", FALSE, NULL);
-    g_main_loop_quit (loop);
-  }
-}
-
-static void
 fakesink_hand_off (GstElement * fakesink, GstBuffer * buf, GstPad * pad,
     gpointer data)
 {
@@ -143,81 +130,6 @@ GST_START_TEST (static_link)
 }
 
 GST_END_TEST
-GST_START_TEST (live_link)
-{
-  GMainLoop *loop = g_main_loop_new (NULL, TRUE);
-  GstElement *pipeline = gst_pipeline_new (NULL);
-  GstElement *videotestsrc = gst_element_factory_make ("videotestsrc", NULL);
-  GstElement *fakesink = gst_element_factory_make ("fakesink", NULL);
-  GstElement *agnosticbin = gst_element_factory_make ("agnosticbin", NULL);
-  GstElement *decoder = gst_element_factory_make ("rtpmp4vpay", NULL);
-  GstElement *outputfakesink = gst_element_factory_make ("fakesink", NULL);
-  gboolean ret;
-
-  GstBus *bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-
-  gst_bus_add_watch (bus, gst_bus_async_signal_func, NULL);
-  g_signal_connect (bus, "message", G_CALLBACK (bus_msg), pipeline);
-  g_object_unref (bus);
-
-  g_object_set (G_OBJECT (fakesink), "sync", TRUE, "signal-handoffs", TRUE,
-      NULL);
-  g_signal_connect (G_OBJECT (fakesink), "handoff",
-      G_CALLBACK (negotiated_fakesink_hand_off), loop);
-
-  g_object_set (G_OBJECT (outputfakesink), "signal-handoffs", TRUE, "sync",
-      TRUE, NULL);
-  g_signal_connect (G_OBJECT (outputfakesink), "handoff",
-      G_CALLBACK (fakesink_hand_off), loop);
-
-  mark_point ();
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
-
-  mark_point ();
-  gst_bin_add_many (GST_BIN (pipeline), videotestsrc, agnosticbin, fakesink,
-      NULL);
-  mark_point ();
-  gst_element_set_state (fakesink, GST_STATE_PLAYING);
-  gst_element_set_state (agnosticbin, GST_STATE_PLAYING);
-  mark_point ();
-  ret = gst_element_link_many (videotestsrc, agnosticbin, fakesink, NULL);
-  fail_unless (ret);
-
-  gst_element_set_state (videotestsrc, GST_STATE_PLAYING);
-
-  g_timeout_add_seconds (30, timeout_check, pipeline);
-
-  mark_point ();
-  g_main_loop_run (loop);
-  mark_point ();
-
-  gst_element_set_state (decoder, GST_STATE_PLAYING);
-  gst_element_set_state (outputfakesink, GST_STATE_PLAYING);
-  gst_bin_add_many (GST_BIN (pipeline), decoder, outputfakesink, NULL);
-
-  mark_point ();
-  ret = gst_element_link (decoder, outputfakesink);
-  fail_unless (ret);
-  ret = gst_element_link (agnosticbin, decoder);
-  fail_unless (ret);
-  mark_point ();
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
-
-  GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
-      GST_DEBUG_GRAPH_SHOW_ALL, "before_entering_loop");
-
-  mark_point ();
-  g_main_loop_run (loop);
-  mark_point ();
-
-  GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
-      GST_DEBUG_GRAPH_SHOW_ALL, "loopback_test_end");
-
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  g_object_unref (pipeline);
-}
-
-GST_END_TEST
 /*
  * End of test cases
  */
@@ -228,7 +140,6 @@ agnostic_suite (void)
   TCase *tc_chain = tcase_create ("element");
 
   suite_add_tcase (s, tc_chain);
-  tcase_add_test (tc_chain, live_link);
   tcase_add_test (tc_chain, static_link);
 
   return s;
