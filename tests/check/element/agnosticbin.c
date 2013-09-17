94957ba56b70539adf04268360531babb01a3fbf
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-09-17 15:31:23 +0200

    Add agnosticbin test to check constant reconfigure events
    Change-Id: I063fb39ea63a3451b4a67fca075f8adeb53df78f

--- tests/check/element/agnosticbin.c
+++ tests/check/element/agnosticbin.c
@@ -24,6 +24,10 @@
 #define AGNOSTIC_KEY "agnostic"
 #define DECODER_KEY "decoder"
 
+#define VALVE_KEY "valve"
+
+static GMainLoop *loop;
+
 static gboolean
 quit_main_loop_idle (gpointer data)
 {
@@ -142,6 +146,124 @@ timeout_check (gpointer pipeline)
   return FALSE;
 }
 
+static gpointer
+toggle_thread (gpointer data)
+{
+  GstElement *pipeline = GST_ELEMENT (data);
+  GstElement *valve = g_object_get_data (G_OBJECT (pipeline), VALVE_KEY);
+  gint i;
+
+  g_usleep (800000);
+
+  for (i = 0; i < 50; i++) {
+    if (g_main_loop_is_running (loop)) {
+      g_object_set (valve, "drop", i % 2, NULL);
+    } else {
+      GST_DEBUG ("Main loop stopped");
+      break;
+    }
+    g_usleep (10000);
+  }
+
+  GST_DEBUG ("Toggle thread finished");
+  g_idle_add (quit_main_loop_idle, loop);
+
+  return NULL;
+}
+
+static void
+bus_msg_valve_test (GstBus * bus, GstMessage * msg, gpointer pipe)
+{
+  switch (msg->type) {
+    case GST_MESSAGE_ERROR:{
+      GST_ERROR ("Error: %P", msg);
+      g_main_loop_quit (loop);
+      GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipe),
+          GST_DEBUG_GRAPH_SHOW_ALL, "error");
+      fail ("Error received on bus");
+      break;
+    }
+    case GST_MESSAGE_WARNING:{
+      GST_WARNING ("Warning: %P", msg);
+      GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipe),
+          GST_DEBUG_GRAPH_SHOW_ALL, "warning");
+      break;
+    }
+    case GST_MESSAGE_STATE_CHANGED:
+    {
+      if (msg->src == pipe) {
+        GstState new_state;
+
+        gst_message_parse_state_changed (msg, NULL, &new_state, NULL);
+
+        if (new_state == GST_STATE_PLAYING) {
+          GThread *thread;
+
+          GST_INFO ("Pipe started: %P", msg);
+
+          thread = g_thread_new ("toggle", toggle_thread, pipe);
+          g_thread_unref (thread);
+        }
+      }
+    }
+      break;
+    default:
+      break;
+  }
+}
+
+GST_START_TEST (valve_test)
+{
+  GstElement *pipeline = gst_pipeline_new (NULL);
+  GstElement *videotestsrc = gst_element_factory_make ("videotestsrc", NULL);
+  GstElement *fakesink = gst_element_factory_make ("fakesink", NULL);
+  GstElement *agnosticbin = gst_element_factory_make ("agnosticbin", NULL);
+  GstElement *valve = gst_element_factory_make ("valve", NULL);
+  GstElement *decoder = gst_element_factory_make ("vp8dec", NULL);
+  GstElement *fakesink2 = gst_element_factory_make ("fakesink", NULL);
+  gboolean ret;
+  GstBus *bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+
+  g_object_set_data (G_OBJECT (pipeline), VALVE_KEY, valve);
+
+  loop = g_main_loop_new (NULL, TRUE);
+  g_object_set (G_OBJECT (pipeline), "async-handling", FALSE, NULL);
+
+  gst_bus_add_watch (bus, gst_bus_async_signal_func, NULL);
+  g_signal_connect (bus, "message", G_CALLBACK (bus_msg_valve_test), pipeline);
+  g_object_unref (bus);
+
+  g_object_set_data (G_OBJECT (fakesink2), DECODER_KEY, decoder);
+  g_object_set_data (G_OBJECT (decoder), AGNOSTIC_KEY, agnosticbin);
+
+  mark_point ();
+  gst_bin_add_many (GST_BIN (pipeline), videotestsrc, agnosticbin, fakesink,
+      valve, decoder, fakesink2, NULL);
+  mark_point ();
+  ret = gst_element_link_many (videotestsrc, agnosticbin, fakesink, NULL);
+  fail_unless (ret);
+  mark_point ();
+  ret = gst_element_link_many (agnosticbin, valve, decoder, fakesink2, NULL);
+  fail_unless (ret);
+  mark_point ();
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
+      GST_DEBUG_GRAPH_SHOW_ALL, "before_entering_loop");
+
+  mark_point ();
+  g_main_loop_run (loop);
+  mark_point ();
+
+  GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
+      GST_DEBUG_GRAPH_SHOW_ALL, "loopback_test_end");
+
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  g_object_unref (pipeline);
+  g_main_loop_unref (loop);
+}
+
+GST_END_TEST
 GST_START_TEST (reconnect_test)
 {
   GMainLoop *loop = g_main_loop_new (NULL, TRUE);
@@ -269,6 +391,7 @@ agnostic_suite (void)
   suite_add_tcase (s, tc_chain);
   tcase_add_test (tc_chain, static_link);
   tcase_add_test (tc_chain, reconnect_test);
+  tcase_add_test (tc_chain, valve_test);
 
   return s;
 }
