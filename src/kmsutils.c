90be86b2397a1178c37c69a2d02c3d94edc72482
Santiago Carot-Nemesio <sancane@gmail.com>
2013-10-17 17:00:50 +0200

    Do not lock the whole stream to set drop property in valve
    Change-Id: I40029fffb150ba5adb3f3f27d5cc944f94911d44

--- src/kmsutils.c
+++ src/kmsutils.c
@@ -26,9 +26,7 @@ kms_utils_set_valve_drop (GstElement * valve, gboolean drop)
 
   sink = gst_element_get_static_pad (valve, "sink");
 
-  GST_PAD_STREAM_LOCK (sink);
   g_object_set (valve, "drop", drop, NULL);
-  GST_PAD_STREAM_UNLOCK (sink);
 
   g_object_unref (sink);
 }
