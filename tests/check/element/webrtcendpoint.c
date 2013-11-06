acacbf55672f0d075bae0d05a14acaa7b6dc2112
Miguel París Díaz <mparisdiaz@gmail.com>
2013-11-06 18:40:17 +0100

    Add test for webrtcendpoint
    Change-Id: Ia588fe019c506ed1e1d71db1d5018d9eaeae4ed8

---
+++ tests/check/element/webrtcendpoint.c
@@ -0,0 +1,129 @@
+/*
+ * (C) Copyright 2013 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include <gst/check/gstcheck.h>
+#include <gst/sdp/gstsdpmessage.h>
+
+static const gchar *pattern_sdp_str = "v=0\r\n"
+    "o=- 0 0 IN IP4 0.0.0.0\r\n"
+    "s=TestSession\r\n"
+    "c=IN IP4 0.0.0.0\r\n"
+    "t=0 0\r\n"
+    "m=video 0 RTP/AVP 96\r\n"
+    "a=rtpmap:96 VP8/90000\r\n"
+    "m=audio 0 RTP/AVP 97\r\n" "a=rtpmap:97 OPUS/48000/1\r\n";
+
+GST_START_TEST (negotiation)
+{
+  GstSDPMessage *pattern_sdp;
+  GstElement *offerer = gst_element_factory_make ("webrtcendpoint", NULL);
+  GstElement *answerer = gst_element_factory_make ("webrtcendpoint", NULL);
+  GstSDPMessage *offer = NULL, *answer = NULL;
+  GstSDPMessage *local_offer = NULL, *local_answer = NULL;
+  gchar *local_offer_str, *local_answer_str;
+  GstSDPMessage *remote_offer = NULL, *remote_answer = NULL;
+  gchar *remote_offer_str, *remote_answer_str;
+  gchar *sdp_str = NULL;
+
+  fail_unless (gst_sdp_message_new (&pattern_sdp) == GST_SDP_OK);
+  fail_unless (gst_sdp_message_parse_buffer ((const guint8 *)
+          pattern_sdp_str, -1, pattern_sdp) == GST_SDP_OK);
+  g_object_set (offerer, "pattern-sdp", pattern_sdp, NULL);
+  fail_unless (gst_sdp_message_free (pattern_sdp) == GST_SDP_OK);
+  g_object_get (offerer, "pattern-sdp", &pattern_sdp, NULL);
+  fail_unless (pattern_sdp != NULL);
+  fail_unless (gst_sdp_message_free (pattern_sdp) == GST_SDP_OK);
+
+  fail_unless (gst_sdp_message_new (&pattern_sdp) == GST_SDP_OK);
+  fail_unless (gst_sdp_message_parse_buffer ((const guint8 *)
+          pattern_sdp_str, -1, pattern_sdp) == GST_SDP_OK);
+  g_object_set (answerer, "pattern-sdp", pattern_sdp, NULL);
+  fail_unless (gst_sdp_message_free (pattern_sdp) == GST_SDP_OK);
+  g_object_get (answerer, "pattern-sdp", &pattern_sdp, NULL);
+  fail_unless (pattern_sdp != NULL);
+  fail_unless (gst_sdp_message_free (pattern_sdp) == GST_SDP_OK);
+
+  g_signal_emit_by_name (offerer, "generate-offer", &offer);
+  fail_unless (offer != NULL);
+  GST_DEBUG ("Offer:\n%s", (sdp_str = gst_sdp_message_as_text (offer)));
+  g_free (sdp_str);
+  sdp_str = NULL;
+
+  g_signal_emit_by_name (answerer, "process-offer", offer, &answer);
+  fail_unless (answer != NULL);
+  GST_DEBUG ("Answer:\n%s", (sdp_str = gst_sdp_message_as_text (answer)));
+  g_free (sdp_str);
+  sdp_str = NULL;
+
+  g_signal_emit_by_name (offerer, "process-answer", answer);
+
+  gst_sdp_message_free (offer);
+  gst_sdp_message_free (answer);
+
+  g_object_get (offerer, "local-offer-sdp", &local_offer, NULL);
+  fail_unless (local_offer != NULL);
+  g_object_get (offerer, "remote-answer-sdp", &remote_answer, NULL);
+  fail_unless (remote_answer != NULL);
+
+  g_object_get (answerer, "remote-offer-sdp", &remote_offer, NULL);
+  fail_unless (remote_offer != NULL);
+  g_object_get (answerer, "local-answer-sdp", &local_answer, NULL);
+  fail_unless (local_answer != NULL);
+
+  local_offer_str = gst_sdp_message_as_text (local_offer);
+  remote_answer_str = gst_sdp_message_as_text (remote_answer);
+
+  remote_offer_str = gst_sdp_message_as_text (remote_offer);
+  local_answer_str = gst_sdp_message_as_text (local_answer);
+
+  GST_DEBUG ("Local offer\n%s", local_offer_str);
+  GST_DEBUG ("Remote answer\n%s", remote_answer_str);
+  GST_DEBUG ("Remote offer\n%s", remote_offer_str);
+  GST_DEBUG ("Local answer\n%s", local_answer_str);
+
+  fail_unless (g_strcmp0 (local_offer_str, remote_offer_str) == 0);
+  fail_unless (g_strcmp0 (remote_answer_str, local_answer_str) == 0);
+
+  g_free (local_offer_str);
+  g_free (remote_answer_str);
+  g_free (local_answer_str);
+  g_free (remote_offer_str);
+
+  gst_sdp_message_free (local_offer);
+  gst_sdp_message_free (remote_answer);
+  gst_sdp_message_free (remote_offer);
+  gst_sdp_message_free (local_answer);
+
+  g_object_unref (offerer);
+  g_object_unref (answerer);
+}
+
+GST_END_TEST
+/*
+ * End of test cases
+ */
+static Suite *
+webrtcendpoint_test_suite (void)
+{
+  Suite *s = suite_create ("webrtcendpoint");
+  TCase *tc_chain = tcase_create ("element");
+
+  suite_add_tcase (s, tc_chain);
+  tcase_add_test (tc_chain, negotiation);
+
+  return s;
+}
+
+GST_CHECK_MAIN (webrtcendpoint_test);
