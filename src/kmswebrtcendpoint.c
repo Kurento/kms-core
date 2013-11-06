1fdcb40bc1e091bf7a556e969111490787738c7f
Miguel París Díaz <mparisdiaz@gmail.com>
2013-11-06 18:39:06 +0100

    webrtcendpoint: Implement set_transport_to_sdp and start_transport_send
    Change-Id: I89eff7c17ae94f78588e2b5d1d7b0b6d2d08242d

--- src/kmswebrtcendpoint.c
+++ src/kmswebrtcendpoint.c
@@ -27,15 +27,40 @@ GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
 G_DEFINE_TYPE (KmsWebrtcEndPoint, kms_webrtc_end_point,
     KMS_TYPE_BASE_RTP_END_POINT);
 
+static gboolean
+kms_webrtc_end_point_set_transport_to_sdp (KmsBaseSdpEndPoint *
+    base_sdp_endpoint, GstSDPMessage * msg)
+{
+  GST_WARNING ("TODO: complete");
+
+  return TRUE;
+}
+
+static void
+kms_webrtc_end_point_start_transport_send (KmsBaseSdpEndPoint *
+    base_rtp_end_point, const GstSDPMessage * offer,
+    const GstSDPMessage * answer, gboolean local_offer)
+{
+  GST_WARNING ("TODO: complete");
+}
+
 static void
 kms_webrtc_end_point_class_init (KmsWebrtcEndPointClass * klass)
 {
+  KmsBaseSdpEndPointClass *base_sdp_end_point_class;
+
   gst_element_class_set_details_simple (GST_ELEMENT_CLASS (klass),
       "WebrtcEndPoint",
       "WEBRTC/Stream/WebrtcEndPoint",
       "WebRTC EndPoint element", "Miguel París Díaz <mparisdiaz@gmail.com>");
 
   GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, PLUGIN_NAME, 0, PLUGIN_NAME);
+
+  base_sdp_end_point_class = KMS_BASE_SDP_END_POINT_CLASS (klass);
+  base_sdp_end_point_class->set_transport_to_sdp =
+      kms_webrtc_end_point_set_transport_to_sdp;
+  base_sdp_end_point_class->start_transport_send =
+      kms_webrtc_end_point_start_transport_send;
 }
 
 static void
