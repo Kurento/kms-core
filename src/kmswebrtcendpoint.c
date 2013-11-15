0ebd716e996863b2e5eda6cc22f966bcfb159fca
Miguel París Díaz <mparisdiaz@gmail.com>
2013-11-15 17:12:03 +0100

    webrtcendpoint: create nice agent
    Change-Id: Id6a191b16be76d1ff4866b385299345eea45eefd

--- src/kmswebrtcendpoint.c
+++ src/kmswebrtcendpoint.c
@@ -38,6 +38,10 @@ G_DEFINE_TYPE (KmsWebrtcEndPoint, kms_webrtc_end_point,
 
 struct _KmsWebrtcEndPointPrivate
 {
+  GMainContext *context;
+  GMainLoop *loop;
+  GThread *thread;
+
   NiceAgent *agent;
 };
 
@@ -59,9 +63,81 @@ kms_webrtc_end_point_start_transport_send (KmsBaseSdpEndPoint *
 }
 
 static void
+gathering_done (NiceAgent * agent, guint stream_id, KmsWebrtcEndPoint * self)
+{
+  GST_WARNING ("TODO: implement");
+}
+
+static gpointer
+loop_thread (gpointer loop)
+{
+  GMainContext *context;
+
+  context = g_main_loop_get_context (loop);
+  g_main_context_acquire (context);
+  g_main_loop_run (loop);
+  g_main_context_release (context);
+
+  return NULL;
+}
+
+static gboolean
+quit_main_loop_idle (gpointer loop)
+{
+  g_main_loop_quit (loop);
+
+  return G_SOURCE_REMOVE;
+}
+
+static void
+destroy_main_loop (gpointer loop)
+{
+  g_main_loop_unref (loop);
+}
+
+static void
+kms_webrtc_end_point_finalize (GObject * object)
+{
+  KmsWebrtcEndPoint *self = KMS_WEBRTC_END_POINT (object);
+
+  if (self->priv->agent != NULL) {
+    g_object_unref (self->priv->agent);
+    self->priv->agent = NULL;
+  }
+
+  if (self->priv->loop != NULL) {
+    GSource *source;
+
+    source = g_idle_source_new ();
+    g_source_set_callback (source, quit_main_loop_idle, self->priv->loop,
+        destroy_main_loop);
+    g_source_attach (source, self->priv->context);
+    g_source_unref (source);
+    self->priv->loop = NULL;
+  }
+
+  if (self->priv->thread != NULL) {
+    g_thread_join (self->priv->thread);
+    g_thread_unref (self->priv->thread);
+  }
+
+  if (self->priv->context != NULL) {
+    g_main_context_unref (self->priv->context);
+    self->priv->context = NULL;
+  }
+
+  /* chain up */
+  G_OBJECT_CLASS (kms_webrtc_end_point_parent_class)->finalize (object);
+}
+
+static void
 kms_webrtc_end_point_class_init (KmsWebrtcEndPointClass * klass)
 {
+  GObjectClass *gobject_class;
   KmsBaseSdpEndPointClass *base_sdp_end_point_class;
+
+  gobject_class = G_OBJECT_CLASS (klass);
+  gobject_class->finalize = kms_webrtc_end_point_finalize;
 
   gst_element_class_set_details_simple (GST_ELEMENT_CLASS (klass),
       "WebrtcEndPoint",
@@ -83,6 +159,32 @@ static void
 kms_webrtc_end_point_init (KmsWebrtcEndPoint * self)
 {
   self->priv = KMS_WEBRTC_END_POINT_GET_PRIVATE (self);
+
+  self->priv->context = g_main_context_new ();
+  if (self->priv->context == NULL) {
+    GST_ERROR_OBJECT (self, "Cannot create context.");
+    return;
+  }
+
+  self->priv->loop = g_main_loop_new (self->priv->context, TRUE);
+  if (self->priv->loop == NULL) {
+    GST_ERROR_OBJECT (self, "Cannot create main loop.");
+    return;
+  }
+
+  self->priv->thread =
+      g_thread_new (GST_ELEMENT_NAME (self), loop_thread, self->priv->loop);
+
+  self->priv->agent =
+      nice_agent_new (self->priv->context, NICE_COMPATIBILITY_RFC5245);
+  if (self->priv->agent == NULL) {
+    GST_ERROR_OBJECT (self, "Cannot create nice agent.");
+    return;
+  }
+
+  g_object_set (self->priv->agent, "upnp", FALSE, NULL);
+  g_signal_connect (self->priv->agent, "candidate-gathering-done",
+      G_CALLBACK (gathering_done), self);
 }
 
 gboolean
