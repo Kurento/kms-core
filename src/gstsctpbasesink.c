d2bb7b36b55918673a13bcfa36246c48832bd7ba
Santiago Carot-Nemesio <sancane@gmail.com>
2014-02-11 16:00:24 +0100

    gstsctpbasesink: Add stream id property
    Change-Id: I6a46011d77d17575831baa701754a5965e7106d4

--- src/gstsctpbasesink.c
+++ src/gstsctpbasesink.c
@@ -21,6 +21,11 @@
 
 #define PLUGIN_NAME "sctpbasesink"
 
+#define STREAM_ID_HIHEST G_MAXUINT16
+#define STREAM_ID_LOWEST 0
+#define DEFAULT_STREAM_ID STREAM_ID_LOWEST
+
+#define SCTP_DEFAULT_PORT 8000
 GST_DEBUG_CATEGORY_STATIC (gst_sctp_base_sink_debug_category);
 #define GST_CAT_DEFAULT gst_sctp_base_sink_debug_category
 
@@ -37,17 +42,72 @@ struct _GstSCTPBaseSinkPrivate
   guint streamid;
 };
 
+enum
+{
+  PROP_0,
+  PROP_STREAM_ID
+};
+
 static GstStaticPadTemplate sinktemplate = GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS_ANY);
 
 static void
+gst_sctp_base_sink_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GstSCTPBaseSink *stcpbasesink;
+
+  g_return_if_fail (GST_IS_SCTP_BASE_SINK (object));
+  stcpbasesink = GST_SCTP_BASE_SINK (object);
+
+  switch (prop_id) {
+    case PROP_STREAM_ID:
+      stcpbasesink->priv->streamid = g_value_get_int (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+gst_sctp_base_sink_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstSCTPBaseSink *stcpbasesink;
+
+  g_return_if_fail (GST_IS_SCTP_BASE_SINK (object));
+  stcpbasesink = GST_SCTP_BASE_SINK (object);
+
+  switch (prop_id) {
+    case PROP_STREAM_ID:
+      g_value_set_int (value, stcpbasesink->priv->streamid);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
 gst_sctp_base_sink_class_init (GstSCTPBaseSinkClass * klass)
 {
   GstElementClass *gstelement_class;
+  GObjectClass *gobject_class;
+
+  gobject_class = G_OBJECT_CLASS (klass);
+  gobject_class->set_property = gst_sctp_base_sink_set_property;
+  gobject_class->get_property = gst_sctp_base_sink_get_property;
 
   gstelement_class = GST_ELEMENT_CLASS (klass);
+
+  g_object_class_install_property (gobject_class, PROP_STREAM_ID,
+      g_param_spec_int ("stream-id", "Stream identifier",
+          "Identifies the stream to which the following user data belongs",
+          STREAM_ID_LOWEST, STREAM_ID_HIHEST, DEFAULT_STREAM_ID,
+          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   gst_element_class_set_static_metadata (gstelement_class,
       "SCTP base sink", "Sink/Network",
