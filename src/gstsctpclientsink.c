a4d26627d5bdc7882235725bf4ac74a0224d6dc9
Santiago Carot-Nemesio <sancane@gmail.com>
2014-02-19 11:41:21 +0100

    gstsctpclientsink: Remove unneded properties
    Change-Id: I5032fa6c5bfe792dd2382da712367bf05a922043

--- src/gstsctpclientsink.c
+++ src/gstsctpclientsink.c
@@ -39,9 +39,6 @@ struct _GstSCTPClientSinkPrivate
 {
   /* socket */
   GSocket *socket;
-  guint streamid;
-  gboolean owner;
-  gboolean close_socket;
   GCancellable *cancellable;
 
   /* server information */
@@ -52,11 +49,8 @@ struct _GstSCTPClientSinkPrivate
 enum
 {
   PROP_0,
-  PROP_CLOSE_SOCKET,
   PROP_HOST,
-  PROP_PORT,
-  PROP_SOCKET,
-  PROP_STREAM_ID
+  PROP_PORT
 };
 
 static GstStaticPadTemplate sinktemplate = GST_STATIC_PAD_TEMPLATE ("sink",
@@ -67,24 +61,22 @@ static GstStaticPadTemplate sinktemplate = GST_STATIC_PAD_TEMPLATE ("sink",
 static void
 gst_sctp_client_sink_destroy_socket (GstSCTPClientSink * self)
 {
+  GError *err = NULL;
+
   if (self->priv->socket == NULL)
     return;
 
-  if (self->priv->owner || self->priv->close_socket) {
-    GError *err = NULL;
+  if (g_socket_is_closed (self->priv->socket)) {
+    GST_DEBUG_OBJECT (self, "Socket is already closed");
+    goto end;
+  }
 
-    if (g_socket_is_closed (self->priv->socket)) {
-      GST_DEBUG_OBJECT (self, "Socket is already closed");
-      goto end;
-    }
+  GST_DEBUG_OBJECT (self, "Closing socket");
 
-    GST_DEBUG_OBJECT (self, "Closing socket");
-
-    if (!g_socket_close (self->priv->socket, &err)) {
-      GST_ERROR ("Failed to close socket %p: %s", self->priv->socket,
-          err->message);
-      g_clear_error (&err);
-    }
+  if (!g_socket_close (self->priv->socket, &err)) {
+    GST_ERROR ("Failed to close socket %p: %s", self->priv->socket,
+        err->message);
+    g_clear_error (&err);
   }
 
 end:
@@ -199,9 +191,6 @@ gst_sctp_client_sink_set_property (GObject * object, guint prop_id,
   self = GST_SCTP_CLIENT_SINK (object);
 
   switch (prop_id) {
-    case PROP_CLOSE_SOCKET:
-      self->priv->close_socket = g_value_get_boolean (value);
-      break;
     case PROP_HOST:
       if (g_value_get_string (value) == NULL) {
         GST_WARNING ("host property cannot be NULL");
@@ -212,14 +201,6 @@ gst_sctp_client_sink_set_property (GObject * object, guint prop_id,
       break;
     case PROP_PORT:
       self->priv->port = g_value_get_int (value);
-      break;
-    case PROP_SOCKET:
-      gst_sctp_client_sink_destroy_socket (self);
-      self->priv->socket = g_value_dup_object (value);
-      GST_DEBUG ("setting socket to %p", self->priv->socket);
-      break;
-    case PROP_STREAM_ID:
-      self->priv->streamid = g_value_get_int (value);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -237,20 +218,11 @@ gst_sctp_client_sink_get_property (GObject * object, guint prop_id,
   self = GST_SCTP_CLIENT_SINK (object);
 
   switch (prop_id) {
-    case PROP_CLOSE_SOCKET:
-      g_value_set_boolean (value, self->priv->close_socket);
-      break;
     case PROP_HOST:
       g_value_set_string (value, self->priv->host);
       break;
     case PROP_PORT:
       g_value_set_int (value, self->priv->port);
-      break;
-    case PROP_SOCKET:
-      g_value_set_object (value, self->priv->socket);
-      break;
-    case PROP_STREAM_ID:
-      g_value_set_int (value, self->priv->streamid);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -283,7 +255,6 @@ gst_sctp_client_sink_start (GstBaseSink * bsink)
   if (self->priv->socket == NULL) {
     gst_sctp_base_sink_create_socket (self);
     g_return_val_if_fail (self->priv->socket != NULL, FALSE);
-    self->priv->owner = TRUE;
   }
 
   return g_socket_is_connected (self->priv->socket);
@@ -338,7 +309,7 @@ gst_sctp_client_sink_render (GstBaseSink * bsink, GstBuffer * buf)
   /* write buffer data */
   while (written < map.size) {
     rret =
-        sctp_socket_send (self->priv->socket, self->priv->streamid,
+        sctp_socket_send (self->priv->socket, 0,
         (gchar *) map.data + written, map.size - written,
         self->priv->cancellable, &err);
 
@@ -388,26 +359,12 @@ gst_sctp_client_sink_class_init (GstSCTPClientSinkClass * klass)
 
   gstelement_class = GST_ELEMENT_CLASS (klass);
 
-  g_object_class_install_property (gobject_class, PROP_CLOSE_SOCKET,
-      g_param_spec_boolean ("close-socket", "Close socket",
-          "Close socket if passed as property on state change",
-          SCTP_DEFAULT_CLOSE_SOCKET,
-          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
   g_object_class_install_property (gobject_class, PROP_HOST,
       g_param_spec_string ("host", "Host", "The host/IP to send the packets to",
           SCTP_DEFAULT_HOST, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
   g_object_class_install_property (gobject_class, PROP_PORT,
       g_param_spec_int ("port", "Port", "The port to send the packets to",
           0, G_MAXUINT16, SCTP_DEFAULT_PORT,
-          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
-  g_object_class_install_property (gobject_class, PROP_SOCKET,
-      g_param_spec_object ("socket", "Socket",
-          "Socket to use for SCTP connection. (NULL == allocate)",
-          G_TYPE_SOCKET, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
-  g_object_class_install_property (gobject_class, PROP_STREAM_ID,
-      g_param_spec_int ("stream-id", "Stream identifier",
-          "Identifies the stream to which the following user data belongs",
-          STREAM_ID_LOWEST, STREAM_ID_HIHEST, DEFAULT_STREAM_ID,
           G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   gst_element_class_set_static_metadata (gstelement_class,
@@ -436,8 +393,6 @@ gst_sctp_client_sink_init (GstSCTPClientSink * self)
   self->priv->cancellable = g_cancellable_new ();
   self->priv->host = g_strdup (SCTP_DEFAULT_HOST);
   self->priv->port = SCTP_DEFAULT_PORT;
-  self->priv->close_socket = SCTP_DEFAULT_CLOSE_SOCKET;
-  self->priv->owner = FALSE;
 }
 
 gboolean
