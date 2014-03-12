d1e5d8d926ffb2f8eebc1afe46e2d192e8b658ec
Santiago Carot-Nemesio <sancane@gmail.com>
2014-03-12 15:05:35 +0100

    kmsaudiomixer: Add sink pads
    Change-Id: I47d233ac6eb15890188308eb46a2fef6b5984da0

--- src/kmsaudiomixer.c
+++ src/kmsaudiomixer.c
@@ -43,7 +43,28 @@ GST_DEBUG_CATEGORY_STATIC (kms_audio_mixer_debug_category);
 struct _KmsAudioMixerPrivate
 {
   GRecMutex mutex;
+
+  guint count;
 };
+
+#define AUDIO_SINK_PAD "sink_%u"
+#define AUDIO_SRC_PAD "src_%u"
+#define RAW_AUDIO_CAPS "audio/x-raw;"
+
+/* the capabilities of the inputs and outputs. */
+static GstStaticPadTemplate audio_sink_factory =
+GST_STATIC_PAD_TEMPLATE (AUDIO_SINK_PAD,
+    GST_PAD_SINK,
+    GST_PAD_REQUEST,
+    GST_STATIC_CAPS (RAW_AUDIO_CAPS)
+    );
+
+static GstStaticPadTemplate audio_src_factory =
+GST_STATIC_PAD_TEMPLATE (AUDIO_SRC_PAD,
+    GST_PAD_SRC,
+    GST_PAD_SOMETIMES,
+    GST_STATIC_CAPS (RAW_AUDIO_CAPS)
+    );
 
 /* class initialization */
 
@@ -69,14 +90,106 @@ kms_audio_mixer_finalize (GObject * object)
 }
 
 static void
+kms_audio_mixer_have_type (GstElement * typefind, guint arg0, GstCaps * caps,
+    gpointer data)
+{
+  KmsAudioMixer *self = KMS_AUDIO_MIXER (data);
+  GstElement *audiorate, *agnosticbin;
+
+  audiorate = gst_element_factory_make ("audiorate", NULL);
+  agnosticbin = gst_element_factory_make ("agnosticbin", NULL);
+  gst_bin_add_many (GST_BIN (self), audiorate, agnosticbin, NULL);
+  gst_element_sync_state_with_parent (audiorate);
+  gst_element_sync_state_with_parent (agnosticbin);
+  gst_element_link_many (typefind, audiorate, agnosticbin, NULL);
+
+  /* TODO:
+   * 1) Link as many agnosticbin's source pads as adder elements we have
+   * 2) Link others agnosticbin sources to the new adder element
+   * 3) Create a new src pad for the new adder element
+   */
+}
+
+static GstPad *
+kms_audio_mixer_request_new_pad (GstElement * element,
+    GstPadTemplate * templ, const gchar * name, const GstCaps * caps)
+{
+  KmsAudioMixer *self = KMS_AUDIO_MIXER (element);
+  GstPad *sinkpad, *pad = NULL;
+  GstElement *typefind;
+  gchar *padname;
+
+  if (templ !=
+      gst_element_class_get_pad_template (GST_ELEMENT_CLASS (G_OBJECT_GET_CLASS
+              (element)), AUDIO_SINK_PAD))
+    return NULL;
+
+  typefind = gst_element_factory_make ("typefind", NULL);
+  sinkpad = gst_element_get_static_pad (typefind, "sink");
+  if (sinkpad == NULL) {
+    gst_object_unref (typefind);
+    return NULL;
+  }
+
+  gst_bin_add (GST_BIN (self), typefind);
+  gst_element_sync_state_with_parent (typefind);
+
+  KMS_AUDIO_MIXER_LOCK (self);
+
+  padname = g_strdup_printf (AUDIO_SINK_PAD, self->priv->count++);
+
+  pad = gst_ghost_pad_new (padname, sinkpad);
+  g_object_unref (sinkpad);
+
+  if (GST_STATE (element) >= GST_STATE_PAUSED
+      || GST_STATE_PENDING (element) >= GST_STATE_PAUSED
+      || GST_STATE_TARGET (element) >= GST_STATE_PAUSED)
+    gst_pad_set_active (pad, TRUE);
+
+  if (!gst_element_add_pad (element, pad)) {
+    GST_ERROR_OBJECT (self, "Could not create pad");
+    g_object_unref (pad);
+    gst_element_set_locked_state (typefind, TRUE);
+    gst_element_set_state (typefind, GST_STATE_NULL);
+    gst_bin_remove (GST_BIN (self), typefind);
+    self->priv->count--;
+    pad = NULL;
+  } else {
+    g_signal_connect (G_OBJECT (typefind), "have-type",
+        G_CALLBACK (kms_audio_mixer_have_type), self);
+  }
+
+  KMS_AUDIO_MIXER_UNLOCK (self);
+  g_free (padname);
+
+  return pad;
+}
+
+static void
+kms_audio_mixer_release_pad (GstElement * element, GstPad * pad)
+{
+  GST_DEBUG ("Remove pad %" GST_PTR_FORMAT, pad);
+}
+
+static void
 kms_audio_mixer_class_init (KmsAudioMixerClass * klass)
 {
+  GstElementClass *gstelement_class = GST_ELEMENT_CLASS (klass);
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
-  gst_element_class_set_static_metadata (GST_ELEMENT_CLASS (klass),
+  gst_element_class_set_static_metadata (gstelement_class,
       "AudioMixer", "Generic",
       "Audio mixer element",
       "Santiago Carot Nemesio <sancane at gmail dot com>");
+
+  gstelement_class->request_new_pad =
+      GST_DEBUG_FUNCPTR (kms_audio_mixer_request_new_pad);
+  gstelement_class->release_pad =
+      GST_DEBUG_FUNCPTR (kms_audio_mixer_release_pad);
+  gst_element_class_add_pad_template (gstelement_class,
+      gst_static_pad_template_get (&audio_sink_factory));
+  gst_element_class_add_pad_template (gstelement_class,
+      gst_static_pad_template_get (&audio_src_factory));
 
   gobject_class->dispose = GST_DEBUG_FUNCPTR (kms_audio_mixer_dispose);
   gobject_class->finalize = GST_DEBUG_FUNCPTR (kms_audio_mixer_finalize);
