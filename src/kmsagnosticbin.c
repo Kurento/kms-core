822b14f96c560cca1123ae686b9cfe75091d8e7e
Santiago Carot-Nemesio <sancane@gmail.com>
2014-03-27 16:52:34 +0100

    kmsagnosticbin: Force cap renogatiation once internal elements have been created
    Change-Id: I162f90d3c9c21b4b0916f9571ccb73f1fb89179b

--- src/kmsagnosticbin.c
+++ src/kmsagnosticbin.c
@@ -867,6 +867,8 @@ kms_agnostic_bin2_configure_input_tee (KmsAgnosticBin2 * self, GstCaps * caps)
 
   self->priv->started = FALSE;
   KMS_AGNOSTIC_BIN2_UNLOCK (self);
+
+  gst_pad_push_event (self->priv->sink, gst_event_new_reconfigure ());
 }
 
 static GstPadProbeReturn
