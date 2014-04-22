654420170c39f6f70ac75c5cf6fc2890f12b24fc
Santiago Carot-Nemesio <sancane@gmail.com>
2014-04-22 09:51:00 +0200

    pointerdetector2: Add test for pointer detector 2 filter
    Change-Id: I8e609f57334a0d36fedf69eb174f128aa5adf1ce

---
+++ tests/check/element/pointerdetector2.c
@@ -0,0 +1,220 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+#include <gst/check/gstcheck.h>
+#include <gst/gst.h>
+#include <glib.h>
+#include <kmstestutils.h>
+
+#include "kmsuriendpointstate.h"
+
+#define TEST_URI "http://ci.kurento.com/imgs/mario-wings.png"
+#define TEST_VIDEO "http://ci.kurento.com/video/small.webm"
+
+GMainLoop *loop;
+
+static void
+configure_structure (GstStructure * buttonsLayout)
+{
+  GstStructure *buttonsLayoutAux;
+  int counter;
+
+  for (counter = 0; counter < 4; counter++) {
+    gchar *id;
+
+    id = g_strdup_printf ("id%d", counter);
+    buttonsLayoutAux = gst_structure_new (id,
+        "upRightCornerX", G_TYPE_INT, 10 + counter,
+        "upRightCornerY", G_TYPE_INT, 35 + counter,
+        "width", G_TYPE_INT, 25 + counter,
+        "height", G_TYPE_INT, 12 + counter,
+        "id", G_TYPE_STRING, id,
+        "inactive_uri", G_TYPE_STRING, TEST_URI,
+        "transparency", G_TYPE_DOUBLE, (double) 0.3,
+        "active_uri", G_TYPE_STRING, TEST_URI, NULL);
+
+    gst_structure_set (buttonsLayout,
+        id, GST_TYPE_STRUCTURE, buttonsLayoutAux, NULL);
+
+    gst_structure_free (buttonsLayoutAux);
+    g_free (id);
+  }
+}
+
+GST_START_TEST (set_properties)
+{
+  GstElement *pointerdetector2;
+  gboolean debug, message, show;
+  GstStructure *buttonsLayout1, *buttonsLayout2, *buttonsLayout3;
+  GstStructure *calibrationArea;
+
+  pointerdetector2 = gst_element_factory_make ("pointerdetector2", NULL);
+
+  debug = TRUE;
+  g_object_set (G_OBJECT (pointerdetector2), "show-debug-region", debug, NULL);
+  g_object_get (G_OBJECT (pointerdetector2), "show-debug-region", &debug, NULL);
+
+  if (debug != TRUE)
+    fail ("unexpected attribute value");
+
+  calibrationArea = gst_structure_new ("calibration_area",
+      "x", G_TYPE_INT, 1,
+      "y", G_TYPE_INT, 2,
+      "width", G_TYPE_INT, 3, "height", G_TYPE_INT, 3, NULL);
+  g_object_set (G_OBJECT (pointerdetector2), "calibration-area",
+      calibrationArea, NULL);
+  gst_structure_free (calibrationArea);
+
+  buttonsLayout1 = gst_structure_new_empty ("windowsLayout1");
+  configure_structure (buttonsLayout1);
+  g_object_set (G_OBJECT (pointerdetector2), "windows-layout", buttonsLayout1,
+      NULL);
+  gst_structure_free (buttonsLayout1);
+
+  buttonsLayout2 = gst_structure_new_empty ("windowsLayout1");
+  configure_structure (buttonsLayout2);
+  g_object_set (G_OBJECT (pointerdetector2), "windows-layout", buttonsLayout2,
+      NULL);
+  gst_structure_free (buttonsLayout2);
+
+  buttonsLayout3 = gst_structure_new_empty ("windowsLayout1");
+  configure_structure (buttonsLayout3);
+  g_object_set (G_OBJECT (pointerdetector2), "windows-layout", buttonsLayout3,
+      NULL);
+  gst_structure_free (buttonsLayout3);
+
+  message = FALSE;
+  g_object_set (G_OBJECT (pointerdetector2), "message", message, NULL);
+  g_object_get (G_OBJECT (pointerdetector2), "message", &message, NULL);
+
+  if (message != FALSE)
+    fail ("unexpected attribute value");
+
+  show = FALSE;
+  g_object_set (G_OBJECT (pointerdetector2), "message", show, NULL);
+  g_object_get (G_OBJECT (pointerdetector2), "message", &show, NULL);
+
+  if (show != FALSE)
+    fail ("unexpected attribute value");
+
+  g_object_unref (pointerdetector2);
+}
+
+GST_END_TEST static void
+bus_msg (GstBus * bus, GstMessage * msg, gpointer pipe)
+{
+  switch (GST_MESSAGE_TYPE (msg)) {
+    case GST_MESSAGE_ERROR:{
+      GST_ERROR ("Error: %" GST_PTR_FORMAT, msg);
+      GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipe),
+          GST_DEBUG_GRAPH_SHOW_ALL, "error");
+      fail ("Error received on bus");
+      break;
+    }
+    case GST_MESSAGE_EOS:{
+      g_main_loop_quit (loop);
+      break;
+    }
+    case GST_MESSAGE_WARNING:{
+      GST_ERROR ("Warning: %" GST_PTR_FORMAT, msg);
+      GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipe),
+          GST_DEBUG_GRAPH_SHOW_ALL, "error");
+      fail ("Warning received on bus");
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+GST_START_TEST (player_with_pointer)
+{
+  GstElement *player, *pipeline, *filter, *fakesink_audio, *fakesink_video;
+  GstStructure *buttonsLayout, *calibrationArea;
+  guint bus_watch_id;
+  GstBus *bus;
+
+  loop = g_main_loop_new (NULL, FALSE);
+  pipeline = gst_pipeline_new ("pipeline_live_stream");
+  g_object_set (G_OBJECT (pipeline), "async-handling", TRUE, NULL);
+  player = gst_element_factory_make ("playerendpoint", NULL);
+  filter = gst_element_factory_make ("pointerdetector2", NULL);
+  fakesink_audio = gst_element_factory_make ("fakesink", NULL);
+  fakesink_video = gst_element_factory_make ("fakesink", NULL);
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+
+  bus_watch_id = gst_bus_add_watch (bus, gst_bus_async_signal_func, NULL);
+  g_signal_connect (bus, "message", G_CALLBACK (bus_msg), pipeline);
+  g_object_unref (bus);
+
+  g_object_set (G_OBJECT (player), "uri", TEST_VIDEO, NULL);
+
+  calibrationArea = gst_structure_new ("calibration_area",
+      "x", G_TYPE_INT, 1,
+      "y", G_TYPE_INT, 2,
+      "width", G_TYPE_INT, 3, "height", G_TYPE_INT, 3, NULL);
+  g_object_set (G_OBJECT (filter), "calibration-area", calibrationArea, NULL);
+  gst_structure_free (calibrationArea);
+
+  buttonsLayout = gst_structure_new_empty ("windowsLayout");
+  configure_structure (buttonsLayout);
+  g_object_set (G_OBJECT (filter), "windows-layout", buttonsLayout, NULL);
+  gst_structure_free (buttonsLayout);
+
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  gst_bin_add (GST_BIN (pipeline), filter);
+  gst_element_set_state (filter, GST_STATE_PLAYING);
+
+  gst_bin_add (GST_BIN (pipeline), fakesink_audio);
+  gst_element_set_state (fakesink_audio, GST_STATE_PLAYING);
+
+  gst_bin_add (GST_BIN (pipeline), fakesink_video);
+  gst_element_set_state (fakesink_video, GST_STATE_PLAYING);
+
+  gst_bin_add (GST_BIN (pipeline), player);
+  gst_element_set_state (player, GST_STATE_PLAYING);
+
+  kms_element_link_pads (player, "audio_src_%u", fakesink_audio, "sink");
+  kms_element_link_pads (player, "video_src_%u", filter, "sink");
+
+  gst_element_link (filter, fakesink_video);
+
+  /* Set player to start state */
+  g_object_set (G_OBJECT (player), "state", KMS_URI_ENDPOINT_STATE_START, NULL);
+
+  g_main_loop_run (loop);
+
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (GST_OBJECT (pipeline));
+  g_source_remove (bus_watch_id);
+  g_main_loop_unref (loop);
+}
+
+GST_END_TEST
+/* Define test suite */
+static Suite *
+pointerdetector2_suite (void)
+{
+  Suite *s = suite_create ("pointerdetector2");
+  TCase *tc_chain = tcase_create ("element");
+
+  suite_add_tcase (s, tc_chain);
+  tcase_add_test (tc_chain, set_properties);
+  tcase_add_test (tc_chain, player_with_pointer);
+
+  return s;
+}
+
+GST_CHECK_MAIN (pointerdetector2);
