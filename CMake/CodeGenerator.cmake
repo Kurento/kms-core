d4a68aba5585afaf1fea4dc690ad52895bc13de4
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-07-08 16:53:52 +0200

    Add debugging options to code generator targets
    Change-Id: I9cf95339a4d74c6c6a0f225557e20ca4c22f551a

--- CMake/CodeGenerator.cmake
+++ CMake/CodeGenerator.cmake
@@ -2,6 +2,9 @@ cmake_minimum_required(VERSION 2.8)
 
 find_package(KtoolRomProcessor REQUIRED)
 
+set (KTOOL_ROM_PROCESSOR_CHECK_FORMAT FALSE CACHE BOOL "Check if codding style of generated code is correct")
+mark_as_advanced(KTOOL_ROM_PROCESSOR_CHECK_FORMAT)
+
 function (generate_sources MODEL_FILE GEN_FILES_DIR TEMPLATES_DIR SOURCE_FILES_OUTPUT HEADER_FILES_OUTPUT)
   message (STATUS "Running code generator")
   execute_process(
@@ -22,6 +25,32 @@ function (generate_sources MODEL_FILE GEN_FILES_DIR TEMPLATES_DIR SOURCE_FILES_O
       elseif(${_FILE} MATCHES ".*hpp")
         list (APPEND GENERATED_HEADER_FILES ${GEN_FILES_DIR}/${_FILE})
       endif ()
+      if (KTOOL_ROM_PROCESSOR_CHECK_FORMAT)
+        if (EXISTS ${GEN_FILES_DIR}/${_FILE}.orig)
+          execute_process (COMMAND rm ${GEN_FILES_DIR}/${_FILE}.orig)
+        endif()
+        execute_process (
+          COMMAND astyle
+            --style=linux
+            --indent=spaces=2
+            --indent-preprocessor
+            --min-conditional-indent=2
+            --break-blocks
+            --pad-oper
+            --pad-paren-out
+            --convert-tabs
+            --align-pointer=name
+            --lineend=linux
+            --break-blocks
+            -q
+            ${GEN_FILES_DIR}/${_FILE}
+        )
+        if (EXISTS ${GEN_FILES_DIR}/${_FILE}.orig)
+          message (WARNING "Style incorrect for file: ${GEN_FILES_DIR}/${_FILE}")
+        endif()
+      endif()
+    else()
+      message (" Generator -> ${_FILE}")
     endif()
   endforeach()
 
