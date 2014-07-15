59d9f3bf2e845fd5e4a6ffb01c2be6a7f3d80a3f
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-07-15 11:06:47 +0200

    Add creation and serializatio test
    Change-Id: I5d407f79b80bbea2b0b76830725ffaa168b8866f

--- tests/server/module.cpp
+++ tests/server/module.cpp
@@ -15,12 +15,21 @@
 
 #include <glibmm/module.h>
 #include <FactoryRegistrar.hpp>
+#include <MediaObjectImpl.hpp>
+#include <KurentoException.hpp>
+#include <jsonrpc/JsonSerializer.hpp>
+#include <Error.hpp>
+#include <gst/gst.h>
+
 
 int
 main (int argc, char **argv)
 {
   const kurento::FactoryRegistrar *registrar;
   void *registrarFactory;
+  std::shared_ptr<kurento::MediaObject> mediaPipeline;
+
+  gst_init (&argc, &argv);
 
   std::string moduleName = "../../src/server/libkmscoremodule.so";
 
@@ -36,16 +45,34 @@ main (int argc, char **argv)
     return 1;
   }
 
-  std::cout << "symbol found" << std::endl;
   registrar = ( (RegistrarFactoryFunc) registrarFactory) ();
   const std::map <std::string, std::shared_ptr <kurento::Factory > > &factories = registrar->getFactories();
 
-  std::cout << "Found " << factories.size() << " factories" << std::endl;
+  mediaPipeline = factories.at ("MediaPipeline")->createObject (Json::Value() );
 
-  for (auto factory : factories) {
-    std::cout << "factory " << factory.second->getName() << std::endl;
+  try {
+    kurento::JsonSerializer writer (true);
+    kurento::JsonSerializer writer2 (true);
+    kurento::JsonSerializer reader (false);
+    kurento::Error e (mediaPipeline, "Error description", 10, "ERROR_TYPE");
+    std::shared_ptr<kurento::Error> e2;
+
+    writer.Serialize ("error", e);
+
+    reader.JsonValue = writer.JsonValue;
+    reader.Serialize ("error", e2);
+
+    writer2.Serialize ("error", e2);
+
+    if (writer.JsonValue.toStyledString() != writer2.JsonValue.toStyledString() ) {
+      std::cerr << "Serialization does not match" << std::endl;
+      return 1;
+    }
+
+  } catch (kurento::KurentoException &e) {
+    std::cerr << "Unexpected exception: " << e.what() << std::endl;
+    return 1;
   }
 
-  // TODO:
   return 0;
 }
