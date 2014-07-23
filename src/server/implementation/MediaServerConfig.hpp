fc779003e76a920acb6a85ca2eaa207601ba2274
David Fernandez <d.fernandezlop@gmail.com>
2014-07-23 15:08:07 +0200

    Add MediaServerConfig class
    Change-Id: Ie9ca94d5d9ae9ddea4d4fe0ede1edaa6964687c1

---
+++ src/server/implementation/MediaServerConfig.hpp
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#ifndef __MEDIA_SERVER_CONFIG_IMPL_HPP__
+#define __MEDIA_SERVER_CONFIG_IMPL_HPP__
+
+#include <gst/sdp/gstsdpmessage.h>
+#include <string>
+
+namespace kurento
+{
+
+class MediaServerConfig
+{
+public:
+  MediaServerConfig (GstSDPMessage *sdpPattern, std::string stunServerAddress,
+             gint stunServerPort, std::string turnURL,
+              std::string pemCertificate)
+  {
+    this->sdpPattern = sdpPattern;
+    this->stunServerAddress = stunServerAddress;
+    this->stunServerPort = stunServerPort;
+    this->turnURL = turnURL;
+    this->pemCertificate = pemCertificate;
+  };
+
+  virtual ~MediaServerConfig() throw () {};
+
+  GstSDPMessage* getSdpPattern () {
+    return this->sdpPattern;
+  };
+
+  std::string getStunServerAddress (){
+    return this->stunServerAddress;
+  };
+
+  gint getStunServerPort (){
+    return this->stunServerPort;
+  };
+
+  std::string getTurnURL (){
+    return this->turnURL;
+  };
+
+  std::string getPemCertificate (){
+    return this->pemCertificate;
+  };
+
+private:
+
+  GstSDPMessage *sdpPattern;
+  std::string stunServerAddress;
+  gint stunServerPort;
+  std::string turnURL;
+  std::string pemCertificate;
+};
+
+} /* kurento */
+
+#endif /* __RTP_CONFIG_IMPL_HPP__ */
