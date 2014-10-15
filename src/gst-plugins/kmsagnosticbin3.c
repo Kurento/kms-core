1353fceebab799e67a275d834ced098b95308fd4
Santiago Carot-Nemesio <sancane@gmail.com>
2014-10-15 16:07:12 +0200

    kmsagnosticbin3: Create src pads
    Change-Id: If13e348d92b61791531dcc22e8e645220ec59320

--- src/gst-plugins/kmsagnosticbin3.c
+++ src/gst-plugins/kmsagnosticbin3.c
@@ -23,6 +23,8 @@
 
 #define PLUGIN_NAME "agnosticbin3"
 
+#define KMS_AGNOSTICBIN3_SRC_PAD_DATA "kms-agnosticbin3-src-pad-data"
+
 #define GST_CAT_DEFAULT kms_agnostic_bin3_debug
 GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
 
@@ -54,6 +56,20 @@ struct _KmsAgnosticBin3Private
   g_rec_mutex_unlock (&KMS_AGNOSTIC_BIN3 (obj)->priv->mutex) \
 )
 
+typedef enum
+{
+  KMS_SRC_PAD_STATE_UNCONFIGURED,
+  KMS_SRC_PAD_STATE_CONFIGURED,
+  KMS_SRC_PAD_STATE_WAITING
+} KmsSrcPadState;
+
+typedef struct _KmsSrcPadData
+{
+  GMutex mutex;
+  KmsSrcPadState state;
+  GstCaps *caps;
+} KmsSrcPadData;
+
 /* Object signals */
 enum
 {
@@ -84,11 +100,45 @@ GST_STATIC_PAD_TEMPLATE (AGNOSTICBIN3_SRC_PAD,
     GST_STATIC_CAPS (KMS_AGNOSTIC_CAPS_CAPS)
     );
 
+static KmsSrcPadData *
+create_src_pad_data ()
+{
+  KmsSrcPadData *data;
+
+  data = g_slice_new0 (KmsSrcPadData);
+  g_mutex_init (&data->mutex);
+  data->state = KMS_SRC_PAD_STATE_UNCONFIGURED;
+
+  return data;
+}
+
+static void
+destroy_src_pad_data (KmsSrcPadData * data)
+{
+  if (data->caps != NULL) {
+    gst_caps_unref (data->caps);
+  }
+
+  g_mutex_clear (&data->mutex);
+  g_slice_free (KmsSrcPadData, data);
+}
+
+static void
+kms_agnostic_bin3_append_pending_src_pad (KmsAgnosticBin3 * self, GstPad * pad)
+{
+  if (GST_STATE (self) >= GST_STATE_PAUSED
+      || GST_STATE_PENDING (self) >= GST_STATE_PAUSED
+      || GST_STATE_TARGET (self) >= GST_STATE_PAUSED) {
+    gst_pad_set_active (pad, TRUE);
+  }
+
+  gst_element_add_pad (GST_ELEMENT (self), pad);
+}
+
 static GstPad *
-kms_agnostic_bin3_request_sink_pad (GstElement * element,
+kms_agnostic_bin3_request_sink_pad (KmsAgnosticBin3 * self,
     GstPadTemplate * templ, const gchar * name, const GstCaps * caps)
 {
-  KmsAgnosticBin3 *self = KMS_AGNOSTIC_BIN3 (element);
   GstElement *agnosticbin;
   GstPad *target, *pad = NULL;
   gchar *padname;
@@ -105,13 +155,13 @@ kms_agnostic_bin3_request_sink_pad (GstElement * element,
 
   pad = gst_ghost_pad_new (padname, target);
 
-  if (GST_STATE (element) >= GST_STATE_PAUSED
-      || GST_STATE_PENDING (element) >= GST_STATE_PAUSED
-      || GST_STATE_TARGET (element) >= GST_STATE_PAUSED) {
+  if (GST_STATE (self) >= GST_STATE_PAUSED
+      || GST_STATE_PENDING (self) >= GST_STATE_PAUSED
+      || GST_STATE_TARGET (self) >= GST_STATE_PAUSED) {
     gst_pad_set_active (pad, TRUE);
   }
 
-  gst_element_add_pad (element, pad);
+  gst_element_add_pad (GST_ELEMENT (self), pad);
 
   g_free (padname);
   g_object_unref (target);
@@ -126,19 +176,140 @@ kms_agnostic_bin3_request_sink_pad (GstElement * element,
   return pad;
 }
 
+static GstElement *
+kms_agnostic_bin3_get_compatible_transcoder_tree (KmsAgnosticBin3 * self,
+    const GstCaps * caps)
+{
+  GstElement *agnostic = NULL;
+  GSList *l;
+
+  KMS_AGNOSTIC_BIN3_LOCK (self);
+
+  for (l = self->priv->agnosticbins; l != NULL; l = l->next) {
+    /* TODO: Check all agnosticbin caps looking for a compatible caps */
+  }
+
+  KMS_AGNOSTIC_BIN3_UNLOCK (self);
+
+  return agnostic;
+}
+
+static GstPad *
+kms_agnostic_bin3_create_new_src_pad (KmsAgnosticBin3 * self,
+    GstPadTemplate * templ)
+{
+  gchar *padname;
+  GstPad *pad;
+
+  padname = g_strdup_printf (AGNOSTICBIN3_SRC_PAD,
+      g_atomic_int_add (&self->priv->src_pad_count, 1));
+  pad = gst_ghost_pad_new_no_target_from_template (padname, templ);
+  g_free (padname);
+
+  return pad;
+}
+
+static GstPad *
+kms_agnostic_bin3_create_src_pad_with_transcodification (KmsAgnosticBin3 * self,
+    GstPadTemplate * templ, const GstCaps * caps)
+{
+  GstPad *pad;
+
+  /* TODO: Check out if an agnosticbin with our caps has been created.  */
+  /* If so, create a ghost pad connected to it. If not, create an empty */
+  /* ghost pad that we will connect when we have input stream */
+
+  pad = kms_agnostic_bin3_create_new_src_pad (self, templ);
+
+  return pad;
+}
+
+static GstPad *
+kms_agnostic_bin3_create_src_pad_with_caps (KmsAgnosticBin3 * self,
+    GstPadTemplate * templ, const gchar * name, const GstCaps * caps)
+{
+  GstElement *element = NULL;
+  KmsSrcPadData *paddata;
+  gboolean ret = FALSE;
+  GstPad *pad;
+
+  element = kms_agnostic_bin3_get_compatible_transcoder_tree (self, caps);
+
+  if (element != NULL) {
+    /* TODO: Create ghost pad connected to the transcoder element */
+    return NULL;
+  }
+
+  paddata = create_src_pad_data ();
+  paddata->caps = gst_caps_copy (caps);
+
+  /* Trigger caps signal */
+  g_signal_emit (G_OBJECT (self), agnosticbin3_signals[SIGNAL_CAPS], 0, caps,
+      &ret);
+
+  if (ret) {
+    /* Someone upstream supports these caps */
+    paddata->state = KMS_SRC_PAD_STATE_WAITING;
+    pad = kms_agnostic_bin3_create_new_src_pad (self, templ);
+  } else {
+    /* Transcode will be done in any available agnosticbin */
+    paddata->state = KMS_SRC_PAD_STATE_CONFIGURED;
+    pad = kms_agnostic_bin3_create_src_pad_with_transcodification (self, templ,
+        caps);
+  }
+
+  g_object_set_data_full (G_OBJECT (pad), KMS_AGNOSTICBIN3_SRC_PAD_DATA,
+      paddata, (GDestroyNotify) destroy_src_pad_data);
+
+  kms_agnostic_bin3_append_pending_src_pad (self, pad);
+
+  return pad;
+}
+
+static GstPad *
+kms_agnostic_bin3_create_src_pad_without_caps (KmsAgnosticBin3 * self,
+    GstPadTemplate * templ, const gchar * name)
+{
+  GstPad *pad;
+
+  pad = kms_agnostic_bin3_create_new_src_pad (self, templ);
+  kms_agnostic_bin3_append_pending_src_pad (self, pad);
+
+  return pad;
+}
+
+static GstPad *
+kms_agnostic_bin3_request_src_pad (KmsAgnosticBin3 * self,
+    GstPadTemplate * templ, const gchar * name, const GstCaps * caps)
+{
+  GstPad *pad;
+
+  if (caps != NULL) {
+    pad = kms_agnostic_bin3_create_src_pad_with_caps (self, templ, name, caps);
+  } else {
+    pad = kms_agnostic_bin3_create_src_pad_without_caps (self, templ, name);
+  }
+
+  return pad;
+}
+
 static GstPad *
 kms_agnostic_bin3_request_new_pad (GstElement * element,
     GstPadTemplate * templ, const gchar * name, const GstCaps * caps)
 {
+  KmsAgnosticBin3 *self = KMS_AGNOSTIC_BIN3 (element);
+
   if (templ ==
       gst_element_class_get_pad_template (GST_ELEMENT_CLASS (G_OBJECT_GET_CLASS
               (element)), AGNOSTICBIN3_SINK_PAD)) {
-    return kms_agnostic_bin3_request_sink_pad (element, templ, name, caps);
+    return kms_agnostic_bin3_request_sink_pad (self, templ, name, caps);
   } else if (templ ==
       gst_element_class_get_pad_template (GST_ELEMENT_CLASS (G_OBJECT_GET_CLASS
               (element)), AGNOSTICBIN3_SRC_PAD)) {
-    return NULL;
+    return kms_agnostic_bin3_request_src_pad (self, templ, name, caps);
   } else {
+    GST_ERROR_OBJECT (element, "Unsupported pad template %" GST_PTR_FORMAT,
+        templ);
     return NULL;
   }
 }
@@ -156,6 +327,7 @@ kms_agnostic_bin3_finalize (GObject * object)
   KmsAgnosticBin3 *self = KMS_AGNOSTIC_BIN3 (object);
 
   g_slist_free (self->priv->agnosticbins);
+
   g_rec_mutex_clear (&self->priv->mutex);
 
   G_OBJECT_CLASS (parent_class)->finalize (object);
--- tests/check/element/agnosticbin3.c
+++ tests/check/element/agnosticbin3.c
@@ -17,11 +17,69 @@
 #include <gst/gst.h>
 #include <glib.h>
 
-GST_START_TEST (create_test)
+static GMainLoop *loop;
+GstElement *pipeline;
+guint finish;
+
+static gboolean
+quit_main_loop_idle (gpointer data)
 {
-  GstElement *pipeline = gst_pipeline_new (__FUNCTION__);
+  g_main_loop_quit (loop);
+  return FALSE;
+}
+
+static gboolean
+print_timedout_pipeline (gpointer data)
+{
+  gchar *name;
+  gchar *pipeline_name;
+
+  pipeline_name = gst_element_get_name (pipeline);
+  name = g_strdup_printf ("%s_timedout", pipeline_name);
+
+  GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
+      GST_DEBUG_GRAPH_SHOW_ALL, name);
+
+  g_free (name);
+  g_free (pipeline_name);
+
+  return FALSE;
+}
+
+static void
+bus_msg (GstBus * bus, GstMessage * msg, gpointer pipe)
+{
+  switch (GST_MESSAGE_TYPE (msg)) {
+    case GST_MESSAGE_ERROR:{
+      gchar *error_file = g_strdup_printf ("error-%s", GST_OBJECT_NAME (pipe));
+
+      GST_ERROR ("Error: %" GST_PTR_FORMAT, msg);
+      GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipe),
+          GST_DEBUG_GRAPH_SHOW_ALL, error_file);
+      g_free (error_file);
+      fail ("Error received on bus");
+      break;
+    }
+    case GST_MESSAGE_WARNING:{
+      gchar *warn_file = g_strdup_printf ("warning-%s", GST_OBJECT_NAME (pipe));
+
+      GST_WARNING ("Warning: %" GST_PTR_FORMAT, msg);
+      GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipe),
+          GST_DEBUG_GRAPH_SHOW_ALL, warn_file);
+      g_free (warn_file);
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+GST_START_TEST (create_sink_pad_test)
+{
   GstElement *agnosticbin = gst_element_factory_make ("agnosticbin3", NULL);
   GstElement *audiotestsrc = gst_element_factory_make ("audiotestsrc", NULL);
+
+  pipeline = gst_pipeline_new (__FUNCTION__);
 
   gst_bin_add_many (GST_BIN (pipeline), audiotestsrc, agnosticbin, NULL);
 
@@ -32,11 +90,98 @@ GST_START_TEST (create_test)
 
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
-  GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS (GST_BIN (pipeline),
-      GST_DEBUG_GRAPH_SHOW_ALL, GST_ELEMENT_NAME (pipeline));
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  g_object_unref (pipeline);
+}
+
+GST_END_TEST
+GST_START_TEST (create_src_pad_test)
+{
+  GstElement *agnosticbin = gst_element_factory_make ("agnosticbin3", NULL);
+  GstElement *sink = gst_element_factory_make ("autoaudiosink", NULL);
+
+  pipeline = gst_pipeline_new (__FUNCTION__);
+
+  gst_bin_add_many (GST_BIN (pipeline), sink, agnosticbin, NULL);
+
+  if (!gst_element_link_pads (agnosticbin, "src_%u", sink, "sink")) {
+    fail ("Could not link elements");
+    return;
+  }
+
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
   gst_element_set_state (pipeline, GST_STATE_NULL);
   g_object_unref (pipeline);
+}
+
+GST_END_TEST static gboolean
+caps_request (GstElement * object, GstCaps * caps, gboolean * success)
+{
+  GST_DEBUG ("Signal catched %" GST_PTR_FORMAT, caps);
+  *success = TRUE;
+
+  if (!g_source_remove (finish)) {
+    GST_ERROR ("Can not remove source");
+  }
+
+  g_idle_add (quit_main_loop_idle, NULL);
+
+  /* No caps supported */
+  return FALSE;
+}
+
+GST_START_TEST (create_src_pad_test_with_caps)
+{
+  GstElement *agnosticbin = gst_element_factory_make ("agnosticbin3", NULL);
+  GstElement *sink = gst_element_factory_make ("avenc_msmpeg4", NULL);
+  GstCaps *caps = gst_caps_from_string ("video/x-raw");
+  GstPadTemplate *templ;
+  GstPad *srcpad, *sinkpad;
+  gboolean success = FALSE;
+  GstBus *bus;
+
+  pipeline = gst_pipeline_new (__FUNCTION__);
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+
+  gst_bus_add_signal_watch (bus);
+  g_signal_connect (bus, "message", G_CALLBACK (bus_msg), pipeline);
+
+  finish = g_timeout_add_seconds (2, quit_main_loop_idle, NULL);
+  g_signal_connect (agnosticbin, "caps", G_CALLBACK (caps_request), &success);
+
+  loop = g_main_loop_new (NULL, TRUE);
+
+  templ =
+      gst_element_class_get_pad_template (GST_ELEMENT_GET_CLASS (agnosticbin),
+      "src_%u");
+
+  gst_bin_add_many (GST_BIN (pipeline), sink, agnosticbin, NULL);
+
+  srcpad = gst_element_request_pad (agnosticbin, templ, NULL, caps);
+  sinkpad = gst_element_get_static_pad (sink, "sink");
+
+  if (gst_pad_link (srcpad, sinkpad) != GST_PAD_LINK_OK) {
+    fail ("Could not link elements");
+  }
+
+  g_object_unref (srcpad);
+  g_object_unref (sinkpad);
+  g_object_unref (templ);
+  gst_caps_unref (caps);
+
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  g_timeout_add_seconds (4, print_timedout_pipeline, NULL);
+
+  g_main_loop_run (loop);
+
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  g_object_unref (bus);
+  g_object_unref (pipeline);
+  g_main_loop_unref (loop);
+
+  fail_unless (success, "No signal catched");
 }
 
 GST_END_TEST
@@ -50,7 +195,10 @@ agnosticbin3_suite (void)
   TCase *tc_chain = tcase_create ("element");
 
   suite_add_tcase (s, tc_chain);
-  tcase_add_test (tc_chain, create_test);
+
+  tcase_add_test (tc_chain, create_sink_pad_test);
+  tcase_add_test (tc_chain, create_src_pad_test_with_caps);
+  tcase_add_test (tc_chain, create_src_pad_test);
 
   return s;
 }
