f3b8e4ca28444b7d66d859edf2bd48fe0a4ac190
Santiago Carot-Nemesio <sancane@gmail.com>
2014-10-22 11:55:38 +0200

    MediaSet: Make deletion of object asynchronous
    Change-Id: Ifa27deff57f8095f26fa1d980118d9ed957cb532

--- src/server/implementation/MediaSet.cpp
+++ src/server/implementation/MediaSet.cpp
@@ -400,6 +400,14 @@ MediaSet::unref (const std::string &sessionId,
   unref (sessionId, object);
 }
 
+static void
+async_delete (MediaObjectImpl *mediaObject, std::string id)
+{
+  GST_DEBUG ("Destroying %s -> %s", mediaObject->getType().c_str(), id.c_str() );
+  /* Deletion of mediaObject weak reference */
+  delete mediaObject;
+}
+
 void MediaSet::releasePointer (MediaObjectImpl *mediaObject)
 {
   std::unique_lock <std::recursive_mutex> lock (recMutex);
@@ -408,8 +416,8 @@ void MediaSet::releasePointer (MediaObjectImpl *mediaObject)
   objectsMap.erase (id );
 
   lock.unlock();
-  GST_DEBUG ("Destroying %s -> %s", mediaObject->getType().c_str(), id.c_str() );
-  delete mediaObject;
+
+  io_service->post ( boost::bind ( async_delete, mediaObject, id ) );
 
   checkEmpty();
 }
