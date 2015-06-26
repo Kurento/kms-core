c98d3965b592fe96a81850ddc04eafa8cbcca163
Santiago Carot-Nemesio <sancane@gmail.com>
2015-06-26 11:16:48 +0200

    Metadata: Add test for propagation of meta data
    Change-Id: Idde88543968be586592a49abd4945ba42075dfc9

--- tests/check/element/metadata.c
+++ tests/check/element/metadata.c
@@ -23,6 +23,127 @@
 
 #include "kmsbufferlacentymeta.h"
 
+#define KMS_FACTORY_MAKE_IF_AVAILABLE(factory_name) ({      \
+  GstElement *_element;                                     \
+  _element = gst_element_factory_make (factory_name, NULL); \
+  if (_element == NULL) {                                   \
+    GST_WARNING ("Can not create element %s. "              \
+      "Test will be canceled", factory_name);               \
+    goto test_canceled;                                     \
+  }                                                         \
+  _element;                                                 \
+})
+
+typedef enum
+{
+  MEDIA_TYPE_AUDIO,
+  MEDIA_TYPE_VIDEO,
+} MediaType;
+
+typedef struct _MetaUnitTest
+{
+  MediaType type;
+  gchar *enc;
+  gchar *pay;
+  gchar *depay;
+  gchar *parser;
+  gchar *dec;
+} MetaUnitTest;
+
+static MetaUnitTest test_cases[] = {
+  {
+        MEDIA_TYPE_VIDEO,
+        "avenc_mpeg4",
+        NULL,                   /*"rtpmp4vpay", */
+        NULL,                   /*"rtpmp4vdepay", */
+        "mpeg4videoparse",
+      "avdec_mpeg4"},
+  {
+        MEDIA_TYPE_VIDEO,
+        "x264enc",
+        "rtph264pay",
+        "rtph264depay",
+        "h264parse",
+      "avdec_h264"},
+  {
+        MEDIA_TYPE_VIDEO,
+        "vp8enc",
+        "rtpvp8pay",
+        "rtpvp8depay",
+        "vp8parse",
+      "vp8dec"},
+  {
+        MEDIA_TYPE_VIDEO,
+        "x264enc",
+        "rtph264pay",
+        "rtph264depay",
+        "h264parse",
+      "avdec_h264"},
+  {
+        MEDIA_TYPE_VIDEO,
+        "x264enc",
+        "rtph264pay",
+        "rtph264depay",
+        "h264parse",
+      "openh264dec"},
+  {
+        MEDIA_TYPE_VIDEO,
+        "openh264enc",
+        "rtph264pay",
+        "rtph264depay",
+        "h264parse",
+      "openh264dec"},
+  {
+        MEDIA_TYPE_VIDEO,
+        "openh264enc",
+        "rtph264pay",
+        "rtph264depay",
+        "h264parse",
+      "avdec_h264"},
+  {
+        MEDIA_TYPE_AUDIO,
+        "opusenc",
+        "rtpopuspay",
+        "rtpopusdepay",
+        "opusparse",
+      "opusdec"},
+  {
+        MEDIA_TYPE_AUDIO,
+        "mulawenc",
+        NULL,
+        NULL,
+        NULL,
+      "mulawdec"},
+  {
+        MEDIA_TYPE_AUDIO,
+        "mulawenc",
+        "rtppcmupay",
+        "rtppcmudepay",
+        NULL,
+      "mulawdec"},
+  {
+        MEDIA_TYPE_AUDIO,
+        "amrnbenc",
+        "rtpamrpay",
+        "rtpamrdepay",
+        NULL,
+      "amrnbdec"},
+  {
+        MEDIA_TYPE_AUDIO,
+        "voamrwbenc",
+        NULL,
+        NULL,
+        NULL,
+      "amrwbdec"},
+  {
+        MEDIA_TYPE_AUDIO,
+        "voamrwbenc",
+        NULL,
+        NULL,
+        NULL,
+      "avdec_amrwb"}
+};
+
 static gboolean
 quit_main_loop (gpointer data)
 {
@@ -67,16 +188,14 @@ bus_msg (GstBus * bus, GstMessage * msg, gpointer pipe)
 static GstPadProbeReturn
 add_metadata_data (GstPad * pad, GstPadProbeInfo * info, gpointer user_data)
 {
+  GstClockTime time;
   GstBuffer *buffer;
-  struct timespec ts;
 
   buffer = GST_PAD_PROBE_INFO_BUFFER (info);
-  if (clock_gettime (CLOCK_MONOTONIC, &ts) < 0) {
-    GST_ERROR_OBJECT (pad, "Can not get time");
-    return GST_PAD_PROBE_OK;
-  }
 
-  kms_buffer_add_buffer_latency_meta (buffer, GST_TIMESPEC_TO_TIME (ts));
+  time = g_get_monotonic_time () * GST_USECOND;
+
+  kms_buffer_add_buffer_latency_meta (buffer, time);
 
   return GST_PAD_PROBE_OK;
 }
@@ -87,26 +206,31 @@ debug_latency_meta (GstPad * pad, GstBuffer * buffer)
   KmsBufferLatencyMeta *meta;
 
   meta = kms_buffer_get_buffer_latency_meta (buffer);
+
   fail_if (meta == NULL);
   fail_if (!GST_CLOCK_TIME_IS_VALID (meta->ts));
 
-  GST_DEBUG_OBJECT (pad, "Meta %" G_GUINT64_FORMAT, meta->ts);
+  GST_LOG_OBJECT (pad, "Meta %" GST_TIME_FORMAT, GST_TIME_ARGS (meta->ts));
 }
 
 static void
 calculate_latency (GstPad * pad, GstBuffer * buffer)
 {
   KmsBufferLatencyMeta *meta;
-  struct timespec ts;
-
-  fail_if (clock_gettime (CLOCK_MONOTONIC, &ts) < 0);
+  GstClockTime now;
 
   meta = kms_buffer_get_buffer_latency_meta (buffer);
+
   fail_if (meta == NULL);
   fail_if (!GST_CLOCK_TIME_IS_VALID (meta->ts));
 
-  GST_INFO_OBJECT (pad, "Latency %" G_GINT64_FORMAT,
-      GST_CLOCK_DIFF (meta->ts, GST_TIMESPEC_TO_TIME (ts)));
+  now = g_get_monotonic_time () * GST_USECOND;
+
+  GST_INFO_OBJECT (pad, "got meta %" GST_TIME_FORMAT " at %" GST_TIME_FORMAT
+      " (diff: %" GST_STIME_FORMAT ")",
+      GST_TIME_ARGS (GST_TIME_AS_USECONDS (meta->ts)),
+      GST_TIME_ARGS (GST_TIME_AS_USECONDS (now)),
+      GST_STIME_ARGS (GST_CLOCK_DIFF (meta->ts, now)));
 }
 
 static GstPadProbeReturn
@@ -132,120 +256,122 @@ handoff_cb (GstElement * fakesink, GstBuffer * buff, GstPad * pad,
   g_signal_handlers_disconnect_by_data (fakesink, user_data);
 }
 
-GST_START_TEST (check_metadata)
+static void
+set_probe_on_pad (GstElement * e, const gchar * pad_name,
+    GstPadProbeCallback callback)
 {
-  GstElement *pipeline, *videotestsrc, *videoconvert, *fakesink;
-  guint bus_watch_id;
-  GMainLoop *loop;
-  GstBus *bus;
   GstPad *pad;
 
-  loop = g_main_loop_new (NULL, FALSE);
-
-  /* Create gstreamer elements */
-  pipeline = gst_pipeline_new ("metadata-test");
-  videotestsrc = gst_element_factory_make ("videotestsrc", NULL);
-  videoconvert = gst_element_factory_make ("videoconvert", NULL);
-  fakesink = gst_element_factory_make ("fakesink", NULL);
-
-  g_object_set (G_OBJECT (videotestsrc), "is-live", TRUE, NULL);
-  g_object_set (G_OBJECT (fakesink), "sync", FALSE, "async", FALSE,
-      "signal-handoffs", TRUE, NULL);
-
-  g_signal_connect (fakesink, "handoff", G_CALLBACK (handoff_cb), loop);
-
-  pad = gst_element_get_static_pad (videotestsrc, "src");
-  gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
-      (GstPadProbeCallback) add_metadata_data, NULL, NULL);
+  pad = gst_element_get_static_pad (e, pad_name);
+  gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER, callback, NULL, NULL);
   g_object_unref (pad);
-
-  pad = gst_element_get_static_pad (videoconvert, "sink");
-  gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
-      (GstPadProbeCallback) show_metadata_data, NULL, NULL);
-  g_object_unref (pad);
-
-  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-
-  bus_watch_id = gst_bus_add_watch (bus, gst_bus_async_signal_func, NULL);
-  g_signal_connect (bus, "message", G_CALLBACK (bus_msg), pipeline);
-  g_object_unref (bus);
-
-  gst_bin_add_many (GST_BIN (pipeline), videotestsrc, videoconvert,
-      fakesink, NULL);
-  gst_element_link_many (videotestsrc, videoconvert, fakesink, NULL);
-
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
-
-  GST_DEBUG ("Test running");
-
-  g_main_loop_run (loop);
-
-  GST_DEBUG ("Stop executed");
-
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (GST_OBJECT (pipeline));
-  GST_DEBUG ("Pipe released");
-
-  g_source_remove (bus_watch_id);
-  g_main_loop_unref (loop);
 }
 
-GST_END_TEST
-GST_START_TEST (check_metadata_enc)
+static void
+check_buffer_latency (MetaUnitTest * test)
 {
-  GstElement *pipeline, *videotestsrc, *videoconvert, *videoenc, *videodec,
-      *fakesink;
+  GstElement *pipeline, *src, *convert, *tee, *queue, *enc, *pay, *depay,
+      *parse, *dec, *fakesink, *tmp;
   guint bus_watch_id;
   GMainLoop *loop;
   GstBus *bus;
-  GstPad *pad;
+  gboolean use_parse, use_payloads;
 
-  loop = g_main_loop_new (NULL, FALSE);
+  src = convert = tee = queue = enc = pay = depay = parse = dec = fakesink =
+      NULL;
 
-  /* Create gstreamer elements */
-  pipeline = gst_pipeline_new ("metadata-test");
-  videotestsrc = gst_element_factory_make ("videotestsrc", NULL);
-  videoconvert = gst_element_factory_make ("videoconvert", NULL);
-  videoenc = gst_element_factory_make ("x264enc", NULL);
-  videodec = gst_element_factory_make ("avdec_h264", NULL);
-  fakesink = gst_element_factory_make ("fakesink", NULL);
+  tee = KMS_FACTORY_MAKE_IF_AVAILABLE ("tee");
+  queue = KMS_FACTORY_MAKE_IF_AVAILABLE ("queue");
 
-  g_object_set (G_OBJECT (videotestsrc), "is-live", TRUE, NULL);
+  switch (test->type) {
+    case MEDIA_TYPE_AUDIO:
+      src = KMS_FACTORY_MAKE_IF_AVAILABLE ("audiotestsrc");
+      convert = KMS_FACTORY_MAKE_IF_AVAILABLE ("audioconvert");
+      break;
+    case MEDIA_TYPE_VIDEO:
+      src = KMS_FACTORY_MAKE_IF_AVAILABLE ("videotestsrc");
+      convert = KMS_FACTORY_MAKE_IF_AVAILABLE ("videoconvert");
+      break;
+    default:
+      fail ("Media type (%d) can not be tested", test->type);
+      return;
+  }
+
+  use_parse = (test->parser != NULL);
+  use_payloads = (test->pay != NULL && test->depay != NULL);
+
+  fail_if (!(use_payloads) && (test->pay != NULL || test->depay != NULL));
+
+  enc = KMS_FACTORY_MAKE_IF_AVAILABLE (test->enc);
+
+  if (use_parse) {
+    parse = KMS_FACTORY_MAKE_IF_AVAILABLE (test->parser);
+  }
+
+  if (use_payloads) {
+    pay = KMS_FACTORY_MAKE_IF_AVAILABLE (test->pay);
+    depay = KMS_FACTORY_MAKE_IF_AVAILABLE (test->depay);
+  }
+
+  dec = KMS_FACTORY_MAKE_IF_AVAILABLE (test->dec);
+  fakesink = KMS_FACTORY_MAKE_IF_AVAILABLE ("fakesink");
+
+  g_object_set (G_OBJECT (src), "is-live", TRUE, NULL);
   g_object_set (G_OBJECT (fakesink), "sync", FALSE, "async", FALSE,
       "signal-handoffs", TRUE, NULL);
 
-  g_signal_connect (fakesink, "handoff", G_CALLBACK (handoff_cb), loop);
+  set_probe_on_pad (src, "src", (GstPadProbeCallback) add_metadata_data);
+  set_probe_on_pad (convert, "sink", (GstPadProbeCallback) show_metadata_data);
+  set_probe_on_pad (tee, "sink", (GstPadProbeCallback) show_metadata_data);
+  set_probe_on_pad (queue, "sink", (GstPadProbeCallback) show_metadata_data);
+  set_probe_on_pad (enc, "sink", (GstPadProbeCallback) show_metadata_data);
+  set_probe_on_pad (dec, "sink", (GstPadProbeCallback) show_metadata_data);
 
-  pad = gst_element_get_static_pad (videotestsrc, "src");
-  gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
-      (GstPadProbeCallback) add_metadata_data, NULL, NULL);
-  g_object_unref (pad);
+  if (use_parse) {
+    set_probe_on_pad (parse, "sink", (GstPadProbeCallback) show_metadata_data);
+  }
 
-  pad = gst_element_get_static_pad (videoconvert, "sink");
-  gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
-      (GstPadProbeCallback) show_metadata_data, NULL, NULL);
-  g_object_unref (pad);
+  if (use_payloads) {
+    set_probe_on_pad (pay, "sink", (GstPadProbeCallback) show_metadata_data);
+    set_probe_on_pad (depay, "sink", (GstPadProbeCallback) show_metadata_data);
+  }
 
-  pad = gst_element_get_static_pad (videoenc, "sink");
-  gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
-      (GstPadProbeCallback) show_metadata_data, NULL, NULL);
-  g_object_unref (pad);
+  loop = g_main_loop_new (NULL, FALSE);
 
-  pad = gst_element_get_static_pad (videodec, "sink");
-  gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
-      (GstPadProbeCallback) show_metadata_data, NULL, NULL);
-  g_object_unref (pad);
-
+  pipeline = gst_pipeline_new ("metadata-test");
   bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
 
   bus_watch_id = gst_bus_add_watch (bus, gst_bus_async_signal_func, NULL);
   g_signal_connect (bus, "message", G_CALLBACK (bus_msg), pipeline);
   g_object_unref (bus);
 
-  gst_bin_add_many (GST_BIN (pipeline), videotestsrc, videoconvert,
-      videoenc, videodec, fakesink, NULL);
-  gst_element_link_many (videotestsrc, videoconvert, videoenc, videodec,
-      fakesink, NULL);
+  g_signal_connect (fakesink, "handoff", G_CALLBACK (handoff_cb), loop);
+
+  gst_bin_add_many (GST_BIN (pipeline), src, convert, tee, queue,
+      enc, dec, fakesink, NULL);
+
+  if (use_payloads) {
+    gst_bin_add_many (GST_BIN (pipeline), pay, depay, NULL);
+  }
+
+  if (use_parse) {
+    gst_bin_add (GST_BIN (pipeline), parse);
+  }
+
+  gst_element_link_many (src, convert, tee, queue, enc, NULL);
+  tmp = enc;
+
+  if (use_payloads) {
+    gst_element_link_many (tmp, pay, depay, NULL);
+    tmp = depay;
+  }
+
+  if (use_parse) {
+    gst_element_link (tmp, parse);
+    tmp = parse;
+  }
+
+  gst_element_link_many (tmp, dec, fakesink, NULL);
 
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
@@ -261,6 +387,30 @@ GST_START_TEST (check_metadata_enc)
 
   g_source_remove (bus_watch_id);
   g_main_loop_unref (loop);
+
+  return;
+
+test_canceled:
+  /* Some test object is missing */
+  g_clear_object (&src);
+  g_clear_object (&convert);
+  g_clear_object (&tee);
+  g_clear_object (&queue);
+  g_clear_object (&enc);
+  g_clear_object (&pay);
+  g_clear_object (&depay);
+  g_clear_object (&parse);
+  g_clear_object (&dec);
+  g_clear_object (&fakesink);
+}
+
+GST_START_TEST (check_metadata_enc)
+{
+  gint i;
+
+  for (i = 0; i < G_N_ELEMENTS (test_cases); i++) {
+    check_buffer_latency (&test_cases[i]);
+  }
 }
 
 GST_END_TEST
@@ -274,9 +424,7 @@ metadata_suite (void)
   TCase *tc_chain = tcase_create ("element");
 
   suite_add_tcase (s, tc_chain);
-  if (FALSE) {
-    tcase_add_test (tc_chain, check_metadata);
-  }
+
   tcase_add_test (tc_chain, check_metadata_enc);
 
   return s;
