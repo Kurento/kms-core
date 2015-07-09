86f6ad26cdeac971d9097c7b64eb16cbdb257757
Santiago Carot-Nemesio <sancane@gmail.com>
2015-07-09 10:28:18 +0200

    kmsutils: Add buffer latency calculation functions
    Change-Id: Id299cf74b48bc633a69c7a9cd3a3370c1caa6c47

--- src/gst-plugins/commons/kmsutils.c
+++ src/gst-plugins/commons/kmsutils.c
@@ -14,6 +14,7 @@
  */
 
 #include "kmsutils.h"
+#include "kmsbufferlacentymeta.h"
 #include "kmsagnosticcaps.h"
 #include <gst/video/video-event.h>
 #include "kmsagnosticcaps.h"
@@ -24,6 +25,65 @@ GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
 #define LAST_KEY_FRAME_REQUEST_TIME "kmslastkeyframe"
 
 #define DEFAULT_KEYFRAME_DISPERSION GST_SECOND  /* 1s */
+
+typedef struct _BufferLatencyData
+{
+  GstPad *pad;
+  BufferLatencyCallback cb;
+  gpointer user_data;
+  GDestroyNotify destroy_data;
+} BufferLatencyData;
+
+typedef struct _BufferLatencyValues
+{
+  gboolean valid;
+  KmsMediaType type;
+} BufferLatencyValues;
+
+static BufferLatencyValues *
+buffer_latency_values_new (gboolean is_valid, KmsMediaType type)
+{
+  BufferLatencyValues *blv;
+
+  blv = g_slice_new (BufferLatencyValues);
+
+  blv->valid = is_valid;
+  blv->type = type;
+
+  return blv;
+}
+
+static void
+buffer_latency_values_destroy (BufferLatencyValues * blv)
+{
+  g_slice_free (BufferLatencyValues, blv);
+}
+
+static BufferLatencyData *
+buffer_latency_data_new (GstPad * pad, BufferLatencyCallback cb,
+    gpointer user_data, GDestroyNotify destroy_data)
+{
+  BufferLatencyData *bl;
+
+  bl = g_slice_new (BufferLatencyData);
+
+  bl->pad = pad;
+  bl->cb = cb;
+  bl->user_data = user_data;
+  bl->destroy_data = destroy_data;
+
+  return bl;
+}
+
+static void
+buffer_latency_data_destroy (BufferLatencyData * bl)
+{
+  if (bl->user_data != NULL && bl->destroy_data != NULL) {
+    bl->destroy_data (bl->user_data);
+  }
+
+  g_slice_free (BufferLatencyData, bl);
+}
 
 void
 kms_utils_set_valve_drop (GstElement * valve, gboolean drop)
@@ -777,6 +837,140 @@ kms_utils_get_time_nsecs ()
 }
 
 /* time end */
+
+static void
+add_buffer_latency_metadata (GstBuffer * buffer)
+{
+  GstClockTime time;
+
+  time = kms_utils_get_time_nsecs ();
+
+  kms_buffer_add_buffer_latency_meta (buffer, time, FALSE, 0);
+}
+
+static gboolean
+add_buffer_list_latency_metadata (GstBuffer ** buffer, guint idx,
+    gpointer user_data)
+{
+  add_buffer_latency_metadata (*buffer);
+
+  return TRUE;
+}
+
+static GstPadProbeReturn
+add_buffer_latency_metadata_cb (GstPad * pad, GstPadProbeInfo * info,
+    gpointer user_data)
+{
+  if (GST_PAD_PROBE_INFO_TYPE (info) & GST_PAD_PROBE_TYPE_BUFFER) {
+    GstBuffer *buffer = GST_PAD_PROBE_INFO_BUFFER (info);
+
+    add_buffer_latency_metadata (buffer);
+  } else if (GST_PAD_PROBE_INFO_TYPE (info) & GST_PAD_PROBE_TYPE_BUFFER_LIST) {
+    GstBufferList *list = GST_PAD_PROBE_INFO_BUFFER_LIST (info);
+
+    gst_buffer_list_foreach (list, add_buffer_list_latency_metadata, NULL);
+  }
+
+  return GST_PAD_PROBE_OK;
+}
+
+gulong
+kms_utils_add_buffer_latency_meta_probe (GstPad * pad, gboolean is_valid,
+    KmsMediaType type)
+{
+  BufferLatencyValues *blv;
+
+  blv = buffer_latency_values_new (is_valid, type);
+
+  return gst_pad_add_probe (pad,
+      GST_PAD_PROBE_TYPE_BUFFER | GST_PAD_PROBE_TYPE_BUFFER_LIST,
+      add_buffer_latency_metadata_cb, blv,
+      (GDestroyNotify) buffer_latency_values_destroy);
+}
+
+static gchar *
+str_media_type (KmsMediaType type)
+{
+  switch (type) {
+    case KMS_MEDIA_TYPE_VIDEO:
+      return "video";
+    case KMS_MEDIA_TYPE_AUDIO:
+      return "audio";
+    case KMS_MEDIA_TYPE_DATA:
+      return "data";
+    default:
+      return "<unsupported>";
+  }
+}
+
+static void
+calculate_buffer_latency (GstBuffer * buffer, BufferLatencyData * bl)
+{
+  KmsBufferLatencyMeta *meta;
+  GstClockTimeDiff diff;
+  GstClockTime now;
+
+  meta = kms_buffer_get_buffer_latency_meta (buffer);
+
+  if (meta == NULL) {
+    return;
+  }
+
+  if (!meta->valid) {
+    /* Ignore this meta */
+    return;
+  }
+
+  now = kms_utils_get_time_nsecs ();
+  diff = GST_CLOCK_DIFF (meta->ts, now);
+
+  if (bl->cb != NULL) {
+    bl->cb (bl->pad, meta->type, diff, bl->user_data);
+  }
+
+  GST_DEBUG_OBJECT (bl->pad, "%s latency %" GST_STIME_FORMAT,
+      str_media_type (meta->type), GST_STIME_ARGS (diff));
+}
+
+static gboolean
+calculate_buffer_list_latency (GstBuffer ** buffer, guint idx,
+    gpointer user_data)
+{
+  calculate_buffer_latency (*buffer, user_data);
+
+  return TRUE;
+}
+
+static GstPadProbeReturn
+calculate_buffer_latency_metadata_cb (GstPad * pad, GstPadProbeInfo * info,
+    gpointer user_data)
+{
+  if (GST_PAD_PROBE_INFO_TYPE (info) & GST_PAD_PROBE_TYPE_BUFFER) {
+    GstBuffer *buffer = GST_PAD_PROBE_INFO_BUFFER (info);
+
+    calculate_buffer_latency (buffer, user_data);
+  } else if (GST_PAD_PROBE_INFO_TYPE (info) & GST_PAD_PROBE_TYPE_BUFFER_LIST) {
+    GstBufferList *list = GST_PAD_PROBE_INFO_BUFFER_LIST (info);
+
+    gst_buffer_list_foreach (list, calculate_buffer_list_latency, user_data);
+  }
+
+  return GST_PAD_PROBE_OK;
+}
+
+gulong
+kms_utils_add_buffer_latency_notification_probe (GstPad * pad,
+    BufferLatencyCallback cb, gpointer user_data, GDestroyNotify destroy_data)
+{
+  BufferLatencyData *bl;
+
+  bl = buffer_latency_data_new (pad, cb, user_data, destroy_data);
+
+  return gst_pad_add_probe (pad,
+      GST_PAD_PROBE_TYPE_BUFFER | GST_PAD_PROBE_TYPE_BUFFER_LIST,
+      calculate_buffer_latency_metadata_cb, bl,
+      (GDestroyNotify) buffer_latency_data_destroy);
+}
 
 static void init_debug (void) __attribute__ ((constructor));
 
--- src/gst-plugins/commons/kmsutils.h
+++ src/gst-plugins/commons/kmsutils.h
@@ -16,6 +16,7 @@
 #define __KMS_UTILS_H__
 
 #include "gst/gst.h"
+#include "kmsmediatype.h"
 
 typedef void (*KmsPadIterationAction) (GstPad * pad, gpointer data);
 typedef void (*KmsPadCallback) (GstPad * pad, gpointer data);
@@ -58,6 +59,11 @@ guint kms_utils_remb_event_manager_get_min (RembEventManager * manager);
 
 /* time */
 GstClockTime kms_utils_get_time_nsecs ();
+
+/* buffer latency */
+typedef void (*BufferLatencyCallback) (GstPad * pad, KmsMediaType type, GstClockTimeDiff t, gpointer user_data);
+gulong kms_utils_add_buffer_latency_meta_probe (GstPad * pad, gboolean is_valid, KmsMediaType type);
+gulong kms_utils_add_buffer_latency_notification_probe (GstPad * pad, BufferLatencyCallback cb, gpointer user_data, GDestroyNotify destroy_data);
 
 /* Type destroying */
 #define KMS_UTILS_DESTROY_H(type) void kms_utils_destroy_##type (type * data);
