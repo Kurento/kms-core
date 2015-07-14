7934588b671c42f9e39fb5eba628eb35331a9c2f
Miguel París Díaz <mparisdiaz@gmail.com>
2015-07-14 14:08:33 +0200

    kmsirtpsessionmananager: interface to abstract session connections
    Change-Id: I6806692b2566b6d657c7536373c7d60f3944f149

---
+++ src/gst-plugins/commons/kmsirtpsessionmanager.c
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include "kmsirtpsessionmanager.h"
+
+G_DEFINE_INTERFACE (KmsIRtpSessionManager, kms_i_rtp_session_manager, 0);
+
+static void
+kms_i_rtp_session_manager_default_init (KmsIRtpSessionManagerInterface * iface)
+{
+  /* do nothing */
+}
+
+GstPad *
+kms_i_rtp_session_manager_request_rtp_sink (KmsIRtpSessionManager * self,
+    KmsBaseRtpSession * sess, SdpMediaConfig * mconf)
+{
+  g_return_val_if_fail (KMS_IS_I_RTP_SESSION_MANAGER (self), NULL);
+
+  return KMS_I_RTP_SESSION_MANAGER_GET_INTERFACE (self)->request_rtp_sink (self,
+      sess, mconf);
+}
+
+GstPad *
+kms_i_rtp_session_manager_request_rtp_src (KmsIRtpSessionManager * self,
+    KmsBaseRtpSession * sess, SdpMediaConfig * mconf)
+{
+  g_return_val_if_fail (KMS_IS_I_RTP_SESSION_MANAGER (self), NULL);
+
+  return KMS_I_RTP_SESSION_MANAGER_GET_INTERFACE (self)->request_rtp_src (self,
+      sess, mconf);
+}
+
+GstPad *
+kms_i_rtp_session_manager_request_rtcp_sink (KmsIRtpSessionManager * self,
+    KmsBaseRtpSession * sess, SdpMediaConfig * mconf)
+{
+  g_return_val_if_fail (KMS_IS_I_RTP_SESSION_MANAGER (self), NULL);
+
+  return
+      KMS_I_RTP_SESSION_MANAGER_GET_INTERFACE (self)->request_rtcp_sink (self,
+      sess, mconf);
+}
+
+GstPad *
+kms_i_rtp_session_manager_request_rtcp_src (KmsIRtpSessionManager * self,
+    KmsBaseRtpSession * sess, SdpMediaConfig * mconf)
+{
+  g_return_val_if_fail (KMS_IS_I_RTP_SESSION_MANAGER (self), NULL);
+
+  return KMS_I_RTP_SESSION_MANAGER_GET_INTERFACE (self)->request_rtcp_src (self,
+      sess, mconf);
+}
---
+++ src/gst-plugins/commons/kmsirtpsessionmanager.h
@@ -0,0 +1,62 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#ifndef __KMS_I_RTP_SESSION_MANAGER_H__
+#define __KMS_I_RTP_SESSION_MANAGER_H__
+
+#include <gst/gst.h>
+#include "kmsbasertpsession.h"
+
+G_BEGIN_DECLS
+
+typedef struct _KmsBaseRtpSession KmsBaseRtpSession;
+
+/* KmsIRtpSessionManager begin */
+#define KMS_TYPE_I_RTP_SESSION_MANAGER \
+  (kms_i_rtp_session_manager_get_type())
+#define KMS_I_RTP_SESSION_MANAGER(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),KMS_TYPE_I_RTP_SESSION_MANAGER,KmsIRtpSessionManager))
+#define KMS_IS_I_RTP_SESSION_MANAGER(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),KMS_TYPE_I_RTP_SESSION_MANAGER))
+#define KMS_I_RTP_SESSION_MANAGER_CAST(obj) ((KmsIRtpSessionManager*)(obj))
+#define KMS_I_RTP_SESSION_MANAGER_GET_INTERFACE(obj) \
+  (G_TYPE_INSTANCE_GET_INTERFACE((obj),KMS_TYPE_I_RTP_SESSION_MANAGER,KmsIRtpSessionManagerInterface))
+#define KMS_I_RTP_SESSION_MANAGER_INTERFACE(iface) ((KmsIRtpSessionManagerInterface *) iface)
+
+typedef struct _KmsIRtpSessionManager KmsIRtpSessionManager;
+typedef struct _KmsIRtpSessionManagerInterface KmsIRtpSessionManagerInterface;
+
+struct _KmsIRtpSessionManagerInterface
+{
+  GTypeInterface parent;
+
+  /* virtual methods */
+  GstPad * (*request_rtp_sink) (KmsIRtpSessionManager *self, KmsBaseRtpSession *sess, SdpMediaConfig *mconf);
+  GstPad * (*request_rtp_src) (KmsIRtpSessionManager *self, KmsBaseRtpSession *sess, SdpMediaConfig *mconf);
+  GstPad * (*request_rtcp_sink) (KmsIRtpSessionManager *self, KmsBaseRtpSession *sess, SdpMediaConfig *mconf);
+  GstPad * (*request_rtcp_src) (KmsIRtpSessionManager *self, KmsBaseRtpSession *sess, SdpMediaConfig *mconf);
+};
+
+GType kms_i_rtp_session_manager_get_type (void);
+
+GstPad * kms_i_rtp_session_manager_request_rtp_sink (KmsIRtpSessionManager *self, KmsBaseRtpSession *sess, SdpMediaConfig *mconf);
+GstPad * kms_i_rtp_session_manager_request_rtp_src (KmsIRtpSessionManager *self, KmsBaseRtpSession *sess, SdpMediaConfig *mconf);
+GstPad * kms_i_rtp_session_manager_request_rtcp_sink (KmsIRtpSessionManager *self, KmsBaseRtpSession *sess, SdpMediaConfig *mconf);
+GstPad * kms_i_rtp_session_manager_request_rtcp_src (KmsIRtpSessionManager *self, KmsBaseRtpSession *sess, SdpMediaConfig *mconf);
+
+/* KmsIRtpSessionManager end */
+
+G_END_DECLS
+#endif /* __KMS_I_RTP_SESSION_MANAGER_H__ */
