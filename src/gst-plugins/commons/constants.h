5bcc3ea1b0870924382bb6384cd62f69e2dc3d51
Miguel París Díaz <mparisdiaz@gmail.com>
2015-11-24 12:32:24 +0100

    constants: define SRTP_REPLAY_WINDOW_SIZE for srtp(enc|dec)
    Change-Id: I6c40e7a0ff476fea19e0d61da1d31211591bf851

--- src/gst-plugins/commons/constants.h
+++ src/gst-plugins/commons/constants.h
@@ -37,6 +37,7 @@ G_BEGIN_DECLS
 
 #define RTCP_MIN_INTERVAL 500 /* ms */
 #define RTP_RTX_SIZE 512 /* packets */
+#define SRTP_REPLAY_WINDOW_SIZE G_MAXINT16 /* packets */
 
 /* rtpbin pad names */
 #define RTPBIN_RECV_RTP_SINK "recv_rtp_sink_"
