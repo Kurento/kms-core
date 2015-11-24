ca5a7d5c48318b342535c27040d24a9576c72759
Santiago Carot-Nemesio <sancane@gmail.com>
2015-11-24 11:47:11 +0100

    kmsrecordingprofile: Add ksr profile
    Change-Id: Ifa5c847b2e419d34adf7dbffb16fa4a5d74b0a0b

--- src/gst-plugins/commons/kmsrecordingprofile.c
+++ src/gst-plugins/commons/kmsrecordingprofile.c
@@ -85,6 +85,55 @@ kms_recording_profile_create_mp4_profile (gboolean has_audio,
   return cprof;
 }
 
+static GstEncodingContainerProfile *
+kms_recording_profile_create_ksr_profile (gboolean has_audio,
+    gboolean has_video)
+{
+  GstEncodingContainerProfile *cprof;
+  GstPadTemplate *templ;
+  GstElement *mux;
+  GstCaps *pc;
+
+  pc = gst_caps_from_string ("application/x-ksr");
+  cprof = gst_encoding_container_profile_new ("Ksr", NULL, pc, NULL);
+  gst_caps_unref (pc);
+
+  /* Use matroska caps to define this profile */
+  mux = gst_element_factory_make ("matroskamux", NULL);
+
+  if (has_audio) {
+    GstCaps *ac;
+
+    templ =
+        gst_element_class_get_pad_template (GST_ELEMENT_GET_CLASS (mux),
+        "audio_%u");
+    ac = gst_pad_template_get_caps (templ);
+
+    gst_encoding_container_profile_add_profile (cprof, (GstEncodingProfile *)
+        gst_encoding_audio_profile_new (ac, NULL, NULL, 0));
+
+    gst_caps_unref (ac);
+  }
+
+  if (has_video) {
+    GstCaps *vc;
+
+    templ =
+        gst_element_class_get_pad_template (GST_ELEMENT_GET_CLASS (mux),
+        "video_%u");
+    vc = gst_pad_template_get_caps (templ);
+
+    gst_encoding_container_profile_add_profile (cprof, (GstEncodingProfile *)
+        gst_encoding_video_profile_new (vc, NULL, NULL, 0));
+
+    gst_caps_unref (vc);
+  }
+
+  g_object_unref (mux);
+
+  return cprof;
+}
+
 GstEncodingContainerProfile *
 kms_recording_profile_create_profile (KmsRecordingProfile profile,
     gboolean has_audio, gboolean has_video)
@@ -102,6 +151,8 @@ kms_recording_profile_create_profile (KmsRecordingProfile profile,
       return kms_recording_profile_create_mp4_profile (FALSE, has_video);
     case KMS_RECORDING_PROFILE_MP4_AUDIO_ONLY:
       return kms_recording_profile_create_mp4_profile (has_audio, FALSE);
+    case KMS_RECORDING_PROFILE_KSR:
+      return kms_recording_profile_create_ksr_profile (has_audio, has_video);
     default:
       GST_WARNING ("Invalid recording profile");
       return NULL;
@@ -127,6 +178,8 @@ kms_recording_profile_supports_type (KmsRecordingProfile profile,
     case KMS_RECORDING_PROFILE_WEBM_AUDIO_ONLY:
     case KMS_RECORDING_PROFILE_MP4_AUDIO_ONLY:
       return type == KMS_ELEMENT_PAD_TYPE_AUDIO;
+    case KMS_RECORDING_PROFILE_KSR:
+      return TRUE;
     default:
       return FALSE;
   }
