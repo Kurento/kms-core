8fc29331f07874e9d603b622e8a5e0173114956a
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2016-05-09 15:29:57 +0200

    MediaSet.cpp: Remove sigc definitions that are already in the library
    Change-Id: Id2cd87a7db4d9a0085f653f661c3d41b1a7a7d87

--- src/server/implementation/MediaSet.cpp
+++ src/server/implementation/MediaSet.cpp
@@ -25,17 +25,6 @@
 /* This is included to avoid problems with slots and lamdas */
 #include <type_traits>
 #include <sigc++/sigc++.h>
-namespace sigc
-{
-template <typename Functor>
-struct functor_trait<Functor, false> {
-  typedef decltype (::sigc::mem_fun (std::declval<Functor &> (),
-                                     &Functor::operator() ) ) _intermediate;
-
-  typedef typename _intermediate::result_type result_type;
-  typedef Functor functor_type;
-};
-}
 
 #define GST_CAT_DEFAULT kurento_media_set
 GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
