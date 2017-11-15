bf87eb2104a21831258dc5e81e5d67a9273b0b7b
Juan Navarro <juan.navarro@gmx.es>
2017-11-15 14:54:03 +0100

    RtpSync tests: Generalize test 'test_sync_two_streams'
    This test, as most others in this test suite, was working on the
    assumption that initial (reference) time was zero.
    In order to make the test more useful and to clarify the different
    concepts about what is the reference time, the real-world NTP clock, and
    the stream timestamps, each value has been separated into different
    arbitrary values.

--- tests/check/general/rtpsync.c
+++ tests/check/general/rtpsync.c
@@ -23,15 +23,15 @@
 
 /* based on rtpjitterbuffer.c */
 static GstBuffer *
-generate_rtp_buffer_full (GstClockTime gst_ts, guint32 ssrc, guint pt,
+generate_rtp_buffer_full (GstClockTime gst_time, guint32 ssrc, guint pt,
     guint seq_num, guint32 rtp_ts)
 {
   GstRTPBuffer rtp = GST_RTP_BUFFER_INIT;
   GstBuffer *buf;
 
   buf = gst_rtp_buffer_new_allocate (0, 0, 0);
-  GST_BUFFER_DTS (buf) = gst_ts;
-  GST_BUFFER_PTS (buf) = gst_ts;
+  GST_BUFFER_DTS (buf) = gst_time;
+  GST_BUFFER_PTS (buf) = gst_time;
 
   gst_rtp_buffer_map (buf, GST_MAP_READWRITE, &rtp);
   gst_rtp_buffer_set_payload_type (&rtp, pt);
@@ -44,7 +44,7 @@ generate_rtp_buffer_full (GstClockTime gst_ts, guint32 ssrc, guint pt,
 }
 
 static GstBuffer *
-generate_rtcp_sr_buffer_full (guint32 ssrc, guint64 ntptime, guint32 rtptime)
+generate_rtcp_sr_buffer_full (guint32 ssrc, guint64 ntp_ts, guint32 rtp_ts)
 {
   GstRTCPBuffer rtcp = GST_RTCP_BUFFER_INIT;
   GstRTCPPacket packet;
@@ -55,60 +55,38 @@ generate_rtcp_sr_buffer_full (guint32 ssrc, guint64 ntptime, guint32 rtptime)
   gst_rtcp_buffer_map (buf, GST_MAP_READWRITE, &rtcp);
   fail_unless (gst_rtcp_buffer_add_packet (&rtcp, GST_RTCP_TYPE_SR,
           &packet) == TRUE);
-  gst_rtcp_packet_sr_set_sender_info (&packet, ssrc, ntptime, rtptime, 0, 0);
+  gst_rtcp_packet_sr_set_sender_info (&packet, ssrc, ntp_ts, rtp_ts, 0, 0);
   gst_rtcp_buffer_unmap (&rtcp);
 
   return buf;
 }
 
-#define process_rtp(sync, gst_ts, ssrc, pt, seq_num, rtp_ts, expected_out_pts, fail_func) \
-do {                                                                                      \
-  GstBuffer *__buf;                                                                       \
-                                                                                          \
-  __buf = generate_rtp_buffer_full (gst_ts, ssrc, pt, seq_num, rtp_ts);                   \
-  GST_DEBUG ("in PTS: %" GST_TIME_FORMAT, GST_TIME_ARGS(GST_BUFFER_PTS (__buf)));         \
-  fail_func (kms_rtp_synchronizer_process_rtp_buffer (sync, __buf, NULL));                \
-  GST_DEBUG ("out PTS: %" GST_TIME_FORMAT, GST_TIME_ARGS(GST_BUFFER_PTS (__buf)));        \
-  fail_unless (GST_BUFFER_PTS (__buf) == expected_out_pts);                               \
-  gst_buffer_unref (__buf);                                                               \
+#define process_rtp(sync, gst_time, ssrc, pt, seq_num, rtp_ts, expected_out_pts, fail_func) \
+do { \
+  GstBuffer *__buf; \
+  __buf = generate_rtp_buffer_full (gst_time, ssrc, pt, seq_num, rtp_ts); \
+  GST_DEBUG ("PTS in:  %" GST_TIME_FORMAT, GST_TIME_ARGS(GST_BUFFER_PTS (__buf))); \
+  fail_func (kms_rtp_synchronizer_process_rtp_buffer (sync, __buf, NULL)); \
+  GST_DEBUG ("PTS out: %" GST_TIME_FORMAT, GST_TIME_ARGS(GST_BUFFER_PTS (__buf))); \
+  GST_DEBUG ("PTS exp: %" GST_TIME_FORMAT, GST_TIME_ARGS(expected_out_pts)); \
+  fail_unless (GST_BUFFER_PTS (__buf) == expected_out_pts); \
+  gst_buffer_unref (__buf); \
 } while (0)
 
-#define process_rtcp(sync, ssrc, ntptime, rtptime, current_time)                            \
-do {                                                                                        \
-  GstBuffer *__buf;                                                                         \
-                                                                                            \
-  __buf = generate_rtcp_sr_buffer_full (0x1, ntptime, rtptime);                             \
-  fail_unless (kms_rtp_synchronizer_process_rtcp_buffer (sync, __buf, current_time, NULL)); \
-  gst_buffer_unref (__buf);                                                                 \
+#define process_rtcp(sync, ssrc, ntp_ts, rtp_ts, current_time) \
+do { \
+  GstBuffer *__buf; \
+  __buf = generate_rtcp_sr_buffer_full (0x1, ntp_ts, rtp_ts); \
+  GST_BUFFER_DTS (__buf) = current_time; \
+  fail_unless (kms_rtp_synchronizer_process_rtcp_buffer (sync, __buf, NULL)); \
+  gst_buffer_unref (__buf); \
 } while (0)
-
-GST_START_TEST (test_sync_context)
-{
-  KmsRtpSyncContext *ctx;
-  GstClockTime ntp_ns_time_out, running_time_out;
-
-  ctx = kms_rtp_sync_context_new (NULL);
-
-  kms_rtp_sync_context_get_time_matching (ctx, 0, 0, &ntp_ns_time_out,
-      &running_time_out);
-  fail_unless (ntp_ns_time_out == 0);
-  fail_unless (running_time_out == 0);
-
-  kms_rtp_sync_context_get_time_matching (ctx, 1, 1, &ntp_ns_time_out,
-      &running_time_out);
-  fail_unless (ntp_ns_time_out == 0);
-  fail_unless (running_time_out == 0);
-
-  g_object_unref (ctx);
-}
-
-GST_END_TEST;
 
 GST_START_TEST (test_sync_add_clock_rate_for_pt)
 {
   KmsRtpSynchronizer *sync;
 
-  sync = kms_rtp_synchronizer_new (NULL, FALSE);
+  sync = kms_rtp_synchronizer_new (FALSE, NULL);
 
   process_rtcp (sync, 0x1, G_GUINT64_CONSTANT (0), 0, 0);
 
@@ -133,7 +111,7 @@ GST_START_TEST (test_sync_one_stream)
 {
   KmsRtpSynchronizer *sync;
 
-  sync = kms_rtp_synchronizer_new (NULL, FALSE);
+  sync = kms_rtp_synchronizer_new (FALSE, NULL);
   fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync, 96, 90000,
           NULL));
 
@@ -163,7 +141,7 @@ GST_START_TEST (test_sync_one_stream_rtptime_after_sr_rtptime)
 {
   KmsRtpSynchronizer *sync;
 
-  sync = kms_rtp_synchronizer_new (NULL, FALSE);
+  sync = kms_rtp_synchronizer_new (FALSE, NULL);
   fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync, 96, 90000,
           NULL));
 
@@ -180,46 +158,61 @@ GST_START_TEST (test_sync_one_stream_rtptime_after_sr_rtptime)
 
   g_object_unref (sync);
 }
-
-GST_END_TEST;
+GST_END_TEST
 
 GST_START_TEST (test_sync_two_streams)
 {
-  KmsRtpSyncContext *ctx;
   KmsRtpSynchronizer *sync_audio, *sync_video;
 
-  ctx = kms_rtp_sync_context_new (NULL);
-  sync_audio = kms_rtp_synchronizer_new (ctx, FALSE);
-  fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync_audio, 0, 8000,
-          NULL));
-  sync_video = kms_rtp_synchronizer_new (ctx, FALSE);
-  fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync_video, 96,
-          90000, NULL));
-  g_object_unref (ctx);
+  const guint32 Aud_ssrc = 0x01;
+  const gint32 Aud_pt = 103;
+  const guint32 Aud_clock = 8000;
+
+  const guint32 Vid_ssrc = 0x02;
+  const gint32 Vid_pt = 96;
+  const guint32 Vid_clock = 90000;
+
+  sync_audio = kms_rtp_synchronizer_new (FALSE, NULL);
+  fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync_audio, Aud_pt,
+      Aud_clock, NULL));
+  sync_video = kms_rtp_synchronizer_new (FALSE, NULL);
+  fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync_video, Vid_pt,
+      Vid_clock, NULL));
+
+  const GstClockTime Bad_time = 42 * GST_SECOND; // Simulates a bad PTS input from a stream
+  const GstClockTime Ref_time = 12 * GST_SECOND; // A reference processing time, such as '00:01:15'
+  const GstClockTime Day_time = 60 * GST_SECOND; // An arbitrary datetime, such as '2017-10-19@13:01:15'
+  const guint64 Day_ntp_ts = gst_util_uint64_scale (Day_time, (1LL << 32),
+      GST_SECOND);
+
+  const int T_ini = 5;
 
   /* Audio RTCP SR */
-  process_rtcp (sync_audio, 0x1, gst_util_uint64_scale (GST_SECOND, (1LL << 32),
-          GST_SECOND), 8000, GST_SECOND);
-  process_rtp (sync_audio, 0, 0x1, 0, 0, 0, 0, fail_unless);    /* Audio frame 0 */
-  process_rtp (sync_audio, 0, 0x1, 0, 1, 8000, GST_SECOND, fail_unless);        /* Audio frame 1 */
+  process_rtcp (sync_audio, Aud_ssrc, Day_ntp_ts, T_ini * Aud_clock, Ref_time);
+  for (int t_now = 0; t_now < 10; t_now++) {
+    process_rtp (sync_audio, t_now * Bad_time, Aud_ssrc, Aud_pt, t_now,
+        t_now * Aud_clock, Ref_time + ((t_now - T_ini) * GST_SECOND),
+        fail_unless);
+  }
 
   /* Video RTCP SR */
-  process_rtcp (sync_video, 0x2, G_GUINT64_CONSTANT (0), 0, GST_SECOND);
-  process_rtp (sync_video, 0, 0x1, 96, 0, 0, 0, fail_unless);   /* Video frame 0 */
-  process_rtp (sync_video, 0, 0x1, 96, 1, 90000, GST_SECOND, fail_unless);      /* Video frame 1 */
-  process_rtp (sync_video, 0, 0x1, 96, 2, 180000, 2 * GST_SECOND, fail_unless); /* Video frame 2 */
+  process_rtcp (sync_video, Vid_ssrc, Day_ntp_ts, T_ini * Vid_clock, Ref_time);
+  for (int t_now = 0; t_now < 10; t_now++) {
+    process_rtp (sync_video, t_now * Bad_time, Vid_ssrc, Vid_pt, t_now,
+        t_now * Vid_clock, Ref_time + ((t_now - T_ini) * GST_SECOND),
+        fail_unless);
+  }
 
   g_object_unref (sync_audio);
   g_object_unref (sync_video);
 }
-
-GST_END_TEST;
+GST_END_TEST
 
 GST_START_TEST (test_sync_avoid_negative_pts)
 {
   KmsRtpSynchronizer *sync;
 
-  sync = kms_rtp_synchronizer_new (NULL, FALSE);
+  sync = kms_rtp_synchronizer_new (FALSE, NULL);
   fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync, 96, 90000,
           NULL));
 
@@ -253,7 +246,7 @@ GST_START_TEST (test_sync_feeded_sorted_but_unsorted)
 {
   KmsRtpSynchronizer *sync;
 
-  sync = kms_rtp_synchronizer_new (NULL, TRUE);
+  sync = kms_rtp_synchronizer_new (TRUE, NULL);
   fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync, 96, 90000,
           NULL));
 
@@ -269,11 +262,11 @@ GST_START_TEST (test_sync_feeded_sorted_rtcp_beetween_same_ts)
 {
   KmsRtpSynchronizer *sync_sorted, *sync_not_sorted;
 
-  sync_sorted = kms_rtp_synchronizer_new (NULL, TRUE);
+  sync_sorted = kms_rtp_synchronizer_new (TRUE, NULL);
   fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync_sorted, 96,
           90000, NULL));
 
-  sync_not_sorted = kms_rtp_synchronizer_new (NULL, FALSE);
+  sync_not_sorted = kms_rtp_synchronizer_new (FALSE, NULL);
   fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync_not_sorted, 96,
           90000, NULL));
 
@@ -298,7 +291,7 @@ GST_START_TEST (test_interpolate)
 {
   KmsRtpSynchronizer *sync;
 
-  sync = kms_rtp_synchronizer_new (NULL, FALSE);
+  sync = kms_rtp_synchronizer_new (FALSE, NULL);
   fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync, 96, 90000,
           NULL));
 
@@ -322,7 +315,7 @@ GST_START_TEST (test_interpolate_avoid_negative_pts)
 {
   KmsRtpSynchronizer *sync;
 
-  sync = kms_rtp_synchronizer_new (NULL, FALSE);
+  sync = kms_rtp_synchronizer_new (FALSE, NULL);
   fail_unless (kms_rtp_synchronizer_add_clock_rate_for_pt (sync, 96, 90000,
           NULL));
 
@@ -351,7 +344,6 @@ rtpsync_suite (void)
   TCase *tc_chain = tcase_create ("general");
 
   suite_add_tcase (s, tc_chain);
-  tcase_add_test (tc_chain, test_sync_context);
 
   tcase_add_test (tc_chain, test_sync_add_clock_rate_for_pt);
   tcase_add_test (tc_chain, test_sync_one_stream);
